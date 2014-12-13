2bd4cbc6c6f2410d99b7309a56a008700612b031
David Fowler <davidfowl@gmail.com>
2014-12-13 02:56:44 -0800

    Implement quick fix endpoint.

--- src/OmniSharp/Api/OmnisharpController.cs
+++ src/OmniSharp/Api/OmnisharpController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Recommendations;
 using Microsoft.CodeAnalysis.Text;
 using OmniSharp.Models;
@@ -18,6 +20,28 @@ namespace OmniSharp
         public OmnisharpController(OmnisharpWorkspace workspace)
         {
             _workspace = workspace;
+        }
+
+        [HttpPost("codecheck")]
+        public async Task<IActionResult> CodeCheck([FromBody]Request request)
+        {
+            EnsureBufferUpdated(request);
+
+            var quickFixes = new List<QuickFix>();
+
+            var documentIds = _workspace.CurrentSolution.GetDocumentIdsWithFilePath(request.FileName);
+
+            var documentId = documentIds.FirstOrDefault();
+
+            if (documentId != null)
+            {
+                var document = _workspace.CurrentSolution.GetDocument(documentId);
+                var semanticModel = await document.GetSemanticModelAsync();
+
+                quickFixes.AddRange(semanticModel.GetDiagnostics().Select(MakeQuickFix));
+            }
+
+            return new ObjectResult(new { QuickFixes = quickFixes });
         }
 
         [HttpPost("autocomplete")]
@@ -56,6 +80,21 @@ namespace OmniSharp
                 var sourceText = SourceText.From(new MemoryStream(buffer), encoding: Encoding.UTF8);
                 _workspace.OnDocumentChanged(documentId, sourceText);
             }
+        }
+
+        private static QuickFix MakeQuickFix(Diagnostic diagnostic)
+        {
+            var span = diagnostic.Location.GetMappedLineSpan();
+            var quickFix = new QuickFix();
+            quickFix.FileName = span.Path;
+            quickFix.Line = span.StartLinePosition.Line + 1;
+            quickFix.Column = span.StartLinePosition.Character;
+            quickFix.EndLine = span.EndLinePosition.Line + 1;
+            quickFix.EndColumn = span.EndLinePosition.Character;
+            quickFix.Text = diagnostic.GetMessage();
+            quickFix.LogLevel = diagnostic.Severity.ToString();
+
+            return quickFix;
         }
     }
 }
---
+++ src/OmniSharp/Models/QuickFix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OmniSharp.Models
+{
+    public class QuickFix
+    {
+        public string LogLevel { get; set; }
+        public string FileName { get; set; }
+        public int Line { get; set; }
+        public int Column { get; set; }
+        public int EndLine { get; set; }
+        public int EndColumn { get; set; }
+        public string Text { get; set; }
+    }
+}
