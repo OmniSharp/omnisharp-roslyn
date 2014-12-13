71b2b4c3f6670ab6e915dadc3249cf6ab6cbbd6d
David Fowler <davidfowl@gmail.com>
2014-12-12 16:10:52 -0800

    Removed redundant AspNet5Context

--- src/OmniSharp/AspNet5/AspNet5Initializer.cs
+++ src/OmniSharp/AspNet5/AspNet5Initializer.cs
@@ -493,54 +493,5 @@ namespace OmniSharp.AspNet5
 
             throw new InvalidOperationException("Unable to locate default alias");
         }
-
-        private class AspNet5Context
-        {
-            private int _contextId;
-
-            public string RuntimePath { get; set; }
-
-            public string HostId { get; private set; }
-
-            public int DesignTimeHostPort { get; set; }
-
-            public Dictionary<string, int> ProjectContextMapping { get; set; }
-
-            public Dictionary<int, Project> Projects { get; set; }
-
-            public Dictionary<ProjectId, FrameworkProject> WorkspaceMapping { get; set; }
-
-            public ProcessingQueue Connection { get; set; }
-
-            public AspNet5Context()
-            {
-                HostId = Guid.NewGuid().ToString();
-                DesignTimeHostPort = 1334;
-                ProjectContextMapping = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
-                Projects = new Dictionary<int, Project>();
-                WorkspaceMapping = new Dictionary<ProjectId, FrameworkProject>();
-            }
-
-            public bool TryAddProject(string projectFile, out int contextId)
-            {
-                contextId = -1;
-                if (ProjectContextMapping.ContainsKey(projectFile))
-                {
-                    return false;
-                }
-
-                contextId = ++_contextId;
-
-                // Create a mapping from path to contextid and back
-                ProjectContextMapping[projectFile] = contextId;
-                Projects[contextId] = new Project
-                {
-                    Path = projectFile,
-                    ContextId = contextId
-                };
-
-                return true;
-            }
-        }
     }
 }
