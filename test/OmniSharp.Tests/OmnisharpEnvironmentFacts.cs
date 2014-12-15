d3ad4a36358a5882b0c4cd8941b4925344ca8b78
David Fowler <davidfowl@gmail.com>
2014-12-15 08:58:42 -0800

    Added unit test project

---
+++ test/OmniSharp.Tests/OmnisharpEnvironmentFacts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Framework.Logging;
+using OmniSharp.Services;
+using Xunit;
+
+namespace OmniSharp.Tests
+{
+    public class OmnisharpEnvironmentFacts
+    {
+        [Fact]
+        public void OmnisharpEnvironmentSetsPathsCorrectly()
+        {
+            var environment = new OmnisharpEnvironment(@"c:\foo\src\foo.sln", TraceType.Information);
+
+            Assert.Equal(@"c:\foo\src\foo.sln", environment.SolutionFilePath);
+            Assert.Equal(@"c:\foo\src", environment.Path);
+        }
+
+        [Fact]
+        public void OmnisharpEnvironmentHasNullSolutionFilePathIfDirectorySet()
+        {
+            var environment = new OmnisharpEnvironment(@"c:\foo\src\", TraceType.Information);
+
+            Assert.Null(environment.SolutionFilePath);
+            Assert.Equal(@"c:\foo\src\", environment.Path);
+        }
+    }
+}
