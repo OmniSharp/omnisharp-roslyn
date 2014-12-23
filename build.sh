65b93357633cdb10c5995044f54e7ec663d4af92
David Fowler <davidfowl@gmail.com>
2014-12-22 19:37:12 -0800

    Check the exit code

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@
 export KRE_FEED=https://www.nuget.org/api/v2
 kvm install 1.0.0-beta1
 kpm restore
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd tests/OmniSharp.Tests
 k test
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd ../../
 export KRE_FEED=https://www.myget.org/F/aspnetvnext/api/v2
 kvm upgrade
 kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-Mono.1.0.0-beta1
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
