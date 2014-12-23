83fb6d7fc392dab066e87dffc8068fb90e219663
David Fowler <davidfowl@gmail.com>
2014-12-22 19:26:26 -0800

    Run tests via build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-export KRE_FEED=https://www.myget.org/F/aspnetvnext/api/v2
-kvm upgrade
 export KRE_FEED=https://www.nuget.org/api/v2
 kvm install 1.0.0-beta1
 kpm restore
+cd tests/OmniSharp.Tests
+k test
+cd ../../
+export KRE_FEED=https://www.myget.org/F/aspnetvnext/api/v2
+kvm upgrade
 kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-Mono.1.0.0-beta1
