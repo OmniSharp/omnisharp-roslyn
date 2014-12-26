900321898761777f8958a532682fbfa9d18d3536
David Fowler <davidfowl@gmail.com>
2014-12-26 14:34:57 -0800

    Fixed build script and dev run script for case sensitive systems

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+if ! type kvm > /dev/null 2>&1; then
+    curl -sSL https://raw.githubusercontent.com/aspnet/Home/master/kvminstall.sh | sh && source ~/.kre/kvm/kvm.sh
+fi
 export KRE_FEED=https://www.nuget.org/api/v2
 kvm install 1.0.0-beta1
 kpm restore
