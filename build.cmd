849cb7609d1087f2ec91862c51372c8595e3abb2
nosami <jasonimison@gmail.com>
2015-01-21 15:09:24 +0000

    update scripts

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@
 set "KRE_NUGET_API_URL=https://www.myget.org/F/aspnetvnext/api/v2"
 CALL kvm upgrade
 set "KRE_NUGET_API_URL=https://www.nuget.org/api/v2"
-CALL kvm install 1.0.0-beta1
-CALL kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-CLR-x86.1.0.0-beta1
+CALL kvm install 1.0.0-beta2
+CALL kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-CLR-x86.1.0.0-beta2
--- build.sh
+++ build.sh
@@ -3,16 +3,15 @@ if ! type kvm > /dev/null 2>&1; then
     curl -sSL https://raw.githubusercontent.com/aspnet/Home/master/kvminstall.sh | sh && source ~/.kre/kvm/kvm.sh
 fi
 export KRE_FEED=https://www.nuget.org/api/v2
-kvm install 1.0.0-beta1
+kvm install 1.0.0-beta2
 kpm restore
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd tests/OmniSharp.Tests
 k test
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd ../../
-export KRE_FEED=https://www.myget.org/F/aspnetvnext/api/v2
 kvm upgrade
-kvm use 1.0.0-beta1
-kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-Mono.1.0.0-beta1 2>&1 | tee errors
+kvm use 1.0.0-beta2
+kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-Mono.1.0.0-beta1 2>&1 | tee buildlog
 # work around for kpm pack returning an exit code 0 on failure 
-grep "Build succeeded" errors
+grep "Build succeeded" buildlog
