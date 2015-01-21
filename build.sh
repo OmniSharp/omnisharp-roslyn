9972ff8111915151fde2ff751e151d6957ac78ae
David Fowler <davidfowl@gmail.com>
2015-01-21 12:03:49 -0800

    Fixed the build again...

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ cd tests/OmniSharp.Tests
 k test
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd ../../
-kvm upgrade
 kvm use 1.0.0-beta2
-kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-Mono.1.0.0-beta1 2>&1 | tee buildlog
+kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-Mono.1.0.0-beta2 2>&1 | tee buildlog
 # work around for kpm pack returning an exit code 0 on failure 
 grep "Build succeeded" buildlog
