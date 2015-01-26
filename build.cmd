9091f82c94f94e08cd08ab450fcf390eca605eba
David Fowler <davidfowl@gmail.com>
2015-01-26 01:11:56 -0800

    Updated build.cmd

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set "KRE_NUGET_API_URL=https://www.myget.org/F/aspnetvnext/api/v2"
 set "KRE_NUGET_API_URL=https://www.nuget.org/api/v2"
 CALL kvm install 1.0.0-beta2
-CALL kpm pack src/OmniSharp --no-source --out artifacts/build/OmniSharp --runtime KRE-CLR-x86.1.0.0-beta2
+CALL kvm use 1.0.0-beta2
+CALL kpm pack src/OmniSharp --no-source --out artifacts/build/omnisharp --runtime KRE-CLR-x86.1.0.0-beta2
