6fafadc26e040dd58c96a96e21bfcc4df65f170d
Johannes Rieken <jrieken@microsoft.com>
2015-01-30 19:29:19 +0100

    more wip - i don't like roslyn's formatter anymore

--- src/OmniSharp/Api/Formatting/OmnisharpController.FormatOnType.cs
+++ src/OmniSharp/Api/Formatting/OmnisharpController.FormatOnType.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.Text;
 using OmniSharp.Models;
+using OmniSharp.Options;
 
 namespace OmniSharp
 {
@@ -15,13 +18,14 @@ namespace OmniSharp
         public async Task<IActionResult> FormatOnType([FromBody]Request request)
         {
             _workspace.EnsureBufferUpdated(request);
-
-            var ret = new FormatRangeResponse();
+            
+            var edits = new List<TextEdit>();
+            var ret = new FormatRangeResponse() { Edits = edits };
 
             var options = _workspace.Options
-                .WithChangedOption(FormattingOptions.NewLine, LanguageNames.CSharp, _options.FormattingOptions.NewLine)
-                .WithChangedOption(FormattingOptions.UseTabs, LanguageNames.CSharp, _options.FormattingOptions.UseTabs)
-                .WithChangedOption(FormattingOptions.TabSize, LanguageNames.CSharp, _options.FormattingOptions.TabSize);
+                .WithChangedOption(Microsoft.CodeAnalysis.Formatting.FormattingOptions.NewLine, LanguageNames.CSharp, _options.FormattingOptions.NewLine)
+                .WithChangedOption(Microsoft.CodeAnalysis.Formatting.FormattingOptions.UseTabs, LanguageNames.CSharp, _options.FormattingOptions.UseTabs)
+                .WithChangedOption(Microsoft.CodeAnalysis.Formatting.FormattingOptions.TabSize, LanguageNames.CSharp, _options.FormattingOptions.TabSize);
 
             var document = _workspace.GetDocument(request.FileName);
             if (document == null)
@@ -36,21 +40,27 @@ namespace OmniSharp
             {
                 return new ObjectResult(ret);
             }
-
-            var linePositionSpan = tree.GetText().Lines.GetLinePositionSpan(target.Span);
-            var formattedNode = Formatter.Format(target, _workspace, options);
-
-            ret.Edits = new TextEdit[] 
-            { 
-                new TextEdit()
-                {
-                    NewText = formattedNode.ToString().Replace("\r\n", _options.FormattingOptions.NewLine + target.GetLeadingTrivia().ToString()),
-                    StartLine = linePositionSpan.Start.Line + 1,
-                    StartColumn = linePositionSpan.Start.Character + 1,
-                    EndLine = linePositionSpan.End.Line + 1,
-                    EndColumn = linePositionSpan.End.Character + 1
-                }
-            };
+            
+            var lines = tree.GetText().Lines;
+            var indent = IndentForNode(target, _options);
+            LinePosition start;
+            LinePosition end;
+            
+            // format the leading trivia
+            if(target.GetLeadingTrivia().ToString() != indent)
+            {   
+                // todo - this will remove leading comment trivia!
+                start = lines.GetLinePosition(target.GetLeadingTrivia().Span.Start);
+                end = lines.GetLinePosition(target.GetLeadingTrivia().Span.End);
+                edits.Add(new TextEdit(indent, start, end));
+            }
+            
+            // format the actual node
+            start = lines.GetLinePosition(target.Span.Start);
+            end = lines.GetLinePosition(target.Span.End);
+            var newText = Formatter.Format(target, _workspace, options).ToString();
+            newText = newText.Replace("\r\n", _options.FormattingOptions.NewLine + indent);
+            edits.Add(new TextEdit(newText, start, end));
 
             return new ObjectResult(ret);
         }
@@ -65,13 +75,51 @@ namespace OmniSharp
             {
                 // ; -> use the statement
                 case SyntaxKind.SemicolonToken:
-                    return token.Parent;
+                    var candiate = token.Parent;
+                    if(candiate.CSharpKind() == SyntaxKind.EmptyStatement) {
+                        return null;
+                    } else {
+                        return candiate;
+                    }
+                    
                 // } -> use the parent of the {}-block
                 case SyntaxKind.CloseBraceToken:
                     return token.Parent.Parent;
             }
 
             return null;
+        }
+        
+        private string IndentForNode(SyntaxNode node, OmniSharpOptions options)
+        {
+            // start with parent
+            node = node.Parent;
+            
+            var indent = 0;
+            while(node != null)
+            {
+                switch(node.CSharpKind())
+                {
+                    case SyntaxKind.NamespaceDeclaration:
+                    case SyntaxKind.ClassDeclaration:
+                    case SyntaxKind.InterfaceDeclaration:
+                    case SyntaxKind.StructDeclaration:
+                    case SyntaxKind.EnumDeclaration:
+                    case SyntaxKind.MethodDeclaration:
+                    case SyntaxKind.SimpleLambdaExpression:
+                    case SyntaxKind.ParenthesizedLambdaExpression:
+                    case SyntaxKind.SetAccessorDeclaration:
+                    case SyntaxKind.GetAccessorDeclaration:
+                        indent += 1;
+                        break;
+                }
+                
+                node = node.Parent;
+            }
+            
+            return options.FormattingOptions.UseTabs 
+                ? new String('\t', indent)
+                : new String(' ', indent * options.FormattingOptions.TabSize);
         }
     }
 }
--- src/OmniSharp/Models/FormatRangeResponse.cs
+++ src/OmniSharp/Models/FormatRangeResponse.cs
@@ -1,9 +1,23 @@
 using System.Collections.Generic;
+using Microsoft.CodeAnalysis.Text;
 
 namespace OmniSharp.Models
 {
 	public class TextEdit
     {
+        public TextEdit() {
+            // empty
+        }
+        
+        public TextEdit(string newText, LinePosition start, LinePosition end)
+        {
+            NewText = newText;
+            StartLine = start.Line + 1;
+            StartColumn = start.Character + 1;
+            EndLine = end.Line + 1;
+            EndColumn = end.Character + 1;
+        }
+        
         public string NewText { get; set; }
         public int StartLine { get; set; }
         public int StartColumn { get; set; }
