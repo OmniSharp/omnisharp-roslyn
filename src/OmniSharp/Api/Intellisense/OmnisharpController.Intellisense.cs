c364652d31552e28bb457af2610837f63bda2886
nosami <jasonimison@gmail.com>
2015-01-31 22:22:52 +0000

    move string extensions to own file

--- src/OmniSharp/Api/Intellisense/OmnisharpController.Intellisense.cs
+++ src/OmniSharp/Api/Intellisense/OmnisharpController.Intellisense.cs
@@ -7,6 +7,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Recommendations;
 using Microsoft.CodeAnalysis.Text;
 using OmniSharp.Models;
+using OmniSharp.Extensions;
 
 namespace OmniSharp
 {
@@ -49,7 +50,6 @@ namespace OmniSharp
                     }
                 }
             }
-
             
             return _completions
                 .OrderByDescending(c => c.CompletionText.IsValidCompletionStartsWithExactCase(wordToComplete))
@@ -144,38 +144,6 @@ namespace OmniSharp
             }
 
             return response;
-        }
-    }
-
-    public static class StringExtensions
-    {
-        public static bool IsValidCompletionFor(this string completion, string partial)
-        {
-            return completion.IsValidCompletionStartsWithIgnoreCase(partial) || completion.IsSubsequenceMatch(partial);
-        }
-
-        public static bool IsValidCompletionStartsWithExactCase(this string completion, string partial)
-        {
-            return completion.StartsWith(partial);
-        }
-
-        public static bool IsValidCompletionStartsWithIgnoreCase(this string completion, string partial)
-        {
-            return completion.ToLower().StartsWith(partial.ToLower());
-        }
-
-        public static bool IsCamelCaseMatch(this string completion, string partial)
-
-        {
-            return new string(completion.Where(c => c >= 'A' && c <= 'Z').ToArray()).StartsWith(partial.ToUpper());
-        }
-
-        public static bool IsSubsequenceMatch(this string completion, string partial)
-        {
-            if (partial == string.Empty)
-                return true;
-
-            return new string(completion.ToUpper().Intersect(partial.ToUpper()).ToArray()) == partial.ToUpper();
         }
     }
 }
