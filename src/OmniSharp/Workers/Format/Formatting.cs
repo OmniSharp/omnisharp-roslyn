41ce69a59651649f6d68fc045ab796f491610ea7
Johannes Rieken <jrieken@microsoft.com>
2015-02-09 12:56:33 +0100

    reflection ftw - use GetFormattedTextChanges to get a proper list of text changes

--- src/OmniSharp/Workers/Format/Formatting.cs
+++ src/OmniSharp/Workers/Format/Formatting.cs
@@ -1,13 +1,37 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Text;
 
 namespace OmniSharp
 {
+    // a little dance so that we can access the ```GetFormattedTextChanges``` method that is hidden by default.
+    // see: https://github.com/dotnet/roslyn/blob/b6484300dfafb43af0c27e542ec457a7583e1aa8/src/Workspaces/Core/Portable/Formatting/Formatter.cs#L309
+    static class FormatterReflect
+    {
+        private static readonly Type formatterType;
+        private static readonly MethodInfo formatMethod;
+
+        static FormatterReflect()
+        {
+            formatterType = typeof(Formatter);
+            formatMethod = formatterType.GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+                .Where(method => method.Name == "GetFormattedTextChanges" && method.GetParameters().Count() == 6)
+                .FirstOrDefault();
+        }
+
+        public static IList<Microsoft.CodeAnalysis.Text.TextChange> GetFormattedTextChanges(SyntaxNode node, TextSpan span, Workspace workspace, OptionSet options = null)
+        {
+            return (IList<Microsoft.CodeAnalysis.Text.TextChange>)formatMethod.Invoke(null, new object[] { node, span, workspace, options, null, null });
+        }
+    }
+
     public class Formatting
     {
         public static async Task<IEnumerable<TextChange>> GetFormattingChangesAfterKeystroke(Workspace workspace, OptionSet options, Document document, int position)
@@ -20,27 +44,23 @@ namespace OmniSharp
                 return Enumerable.Empty<TextChange>();
             }
 
-            // Instead of formatting the target node, we annotate the target node and format the
-            // whole compilation unit and -using an annotation- find the formatted node in the
-            // new syntax tree. That way we get the proper indentation for free.
-            var annotation = new SyntaxAnnotation("formatOnTypeHelper");
-            var newRoot = tree.GetRoot().ReplaceNode(target, target.WithAdditionalAnnotations(annotation));
-            var formatted = Formatter.Format(newRoot, target.FullSpan, workspace, options);
-
-            var node = formatted.GetAnnotatedNodes(annotation).FirstOrDefault();
-            if (node == null)
+            var changes = FormatterReflect.GetFormattedTextChanges(tree.GetRoot(), target.FullSpan, workspace, options);
+            var lines = tree.GetText().Lines;
+            var result = changes.Select(change =>
             {
-                return Enumerable.Empty<TextChange>();
-            }
+                var linePositionSpan = lines.GetLinePositionSpan(change.Span);
+                var newText = EnsureProperNewLine(change.NewText, options);
 
-            var linePositionSpan = tree.GetText().Lines.GetLinePositionSpan(target.FullSpan);
-            var newText = node.ToFullString();
-            newText = EnsureProperNewLine(newText, options);
-
-            return new TextChange[]
-            {
-                new TextChange(newText, linePositionSpan.Start, linePositionSpan.End)
-            };
+                return new TextChange()
+                {
+                    NewText = newText,
+                    StartLine = linePositionSpan.Start.Line + 1,
+                    StartColumn = linePositionSpan.Start.Character + 1,
+                    EndLine = linePositionSpan.End.Line + 1,
+                    EndColumn = linePositionSpan.End.Character + 1
+                };
+            });
+            return result;
         }
 
         private static string EnsureProperNewLine(string text, OptionSet options)
--- src/OmniSharp/Workers/Format/TextChange.cs
+++ src/OmniSharp/Workers/Format/TextChange.cs
@@ -4,20 +4,6 @@ namespace OmniSharp
 {
     public class TextChange
     {
-        public TextChange()
-        {
-            // empty
-        }
-
-        public TextChange(string newText, LinePosition start, LinePosition end)
-        {
-            NewText = newText;
-            StartLine = start.Line + 1;
-            StartColumn = start.Character + 1;
-            EndLine = end.Line + 1;
-            EndColumn = end.Character + 1;
-        }
-
         public string NewText { get; set; }
         public int StartLine { get; set; }
         public int StartColumn { get; set; }
