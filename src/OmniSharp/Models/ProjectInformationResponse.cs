f1dab2ce771dac50fcf4485d5c7da9048ef12ec8
Hugo CARNICELLI <hugocarnicelli@gmail.com>
2015-02-14 18:42:32 +0100

    tabs
    fixing tabs

--- src/OmniSharp/Models/ProjectInformationResponse.cs
+++ src/OmniSharp/Models/ProjectInformationResponse.cs
@@ -3,9 +3,9 @@ using OmniSharp.MSBuild.ProjectFile;
 
 namespace OmniSharp.Models
 {
-	public class ProjectInformationResponse
-	{
-		public MSBuildProject MsBuildProject { get; set; }
-		public AspNet5Project AspNet5Project { get; set; }
-	}
+    public class ProjectInformationResponse
+    {
+        public MSBuildProject MsBuildProject { get; set; }
+        public AspNet5Project AspNet5Project { get; set; }
+    }
 }
