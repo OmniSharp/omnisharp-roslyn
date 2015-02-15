92e07ecc207d0ae36396671f885f8f9e8aa62787
Hugo CARNICELLI <hugocarnicelli@gmail.com>
2015-02-15 16:40:07 +0100

    tests
    separated the 3 tests on projectName, path and commands

--- tests/OmniSharp.Tests/ProjectFact.cs
+++ tests/OmniSharp.Tests/ProjectFact.cs
@@ -11,10 +11,30 @@ namespace OmniSharp.Tests
         public void ProjectCommands()
         {
             var commands = GetCommands();
-
+            var projectName = GetProjectName();
+            var path = GetPath();
             Assert.Equal(1, commands.Count);
+            Assert.Equal("OmniSharp", projectName);
+            Assert.Equal("project.json", path);
         }
-
+        private string GetProjectName(string source = "")
+        {
+            var workspace = TestHelpers.CreateSimpleWorkspace(source);
+            var aspnet5Context = GetAspNet5Context();
+            var controller = new ProjectSystemController(aspnet5Context, null, workspace);
+            var request = CreateRequest(source);
+            var response = controller.CurrentProject(request);
+            return response.AspNet5Project.Name;
+        }
+        private string GetPath(string source = "")
+        {
+            var workspace = TestHelpers.CreateSimpleWorkspace(source);
+            var aspnet5Context = GetAspNet5Context();
+            var controller = new ProjectSystemController(aspnet5Context, null, workspace);
+            var request = CreateRequest(source);
+            var response = controller.CurrentProject(request);
+            return response.AspNet5Project.Path;
+        }
         private IDictionary<string, string> GetCommands(string source = "")
         {
             var workspace = TestHelpers.CreateSimpleWorkspace(source);
@@ -22,11 +42,7 @@ namespace OmniSharp.Tests
             var controller = new ProjectSystemController(aspnet5Context, null, workspace);
             var request = CreateRequest(source);
             var response = controller.CurrentProject(request);
-            if (response.AspNet5Project != null)
-            {
-                return response.AspNet5Project.Commands;
-            }
-            return new Dictionary<string, string>();
+            return response.AspNet5Project.Commands;
         }
 
         private AspNet5Context GetAspNet5Context()
