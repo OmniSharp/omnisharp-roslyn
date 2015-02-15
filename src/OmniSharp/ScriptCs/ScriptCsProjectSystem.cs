37b1aed0dadf4a32a2c7e5b870496814c26ab0eb
filipw <wojcieszyn@gmail.com>
2015-02-15 15:39:20 +0100

    updated to roughly support script packs
    Conflicts:
    	src/OmniSharp/project.json

--- src/OmniSharp/ScriptCs/ScriptCsProjectSystem.cs
+++ src/OmniSharp/ScriptCs/ScriptCsProjectSystem.cs
@@ -11,14 +11,49 @@ using ScriptCs;
 using Common.Logging;
 using ScriptCs.Contracts;
 using System.Collections.Generic;
+using ScriptCs.Hosting;
+using LogLevel = ScriptCs.Contracts.LogLevel;
+using System;
+using Common.Logging.Simple;
+using Autofac;
 
 namespace OmniSharp.ScriptCs
 {
+    public class NullScriptEngine : IScriptEngine
+    {
+        public string BaseDirectory
+        {
+            get;
+
+            set;
+        }
+
+        public string CacheDirectory
+        {
+            get;
+
+            set;
+        }
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        public ScriptResult Execute(string code, string[] scriptArgs, AssemblyReferences references, IEnumerable<string> namespaces, ScriptPackSession scriptPackSession)
+        {
+            return new ScriptResult();
+        }
+    }
+
     public class ScriptCsProjectSystem : IProjectSystem
     {
+        private static string baseAssemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
         private readonly OmnisharpWorkspace _workspace;
         private readonly IOmnisharpEnvironment _env;
         private readonly ILogger _logger;
+        private ScriptServices _scriptServices;
 
         public ScriptCsProjectSystem(OmnisharpWorkspace workspace, IOmnisharpEnvironment env, ILoggerFactory loggerFactory)
         {
@@ -37,36 +72,74 @@ namespace OmniSharp.ScriptCs
             {
                 _logger.WriteInformation(string.Format("Using CSX files at '{0}'.", csxPath));
 
-                var parseOptions = new CSharpParseOptions(LanguageVersion.CSharp6, DocumentationMode.Parse,
-    SourceCodeKind.Script);
-                var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+                LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter();
+                var scriptcsLogger = LogManager.GetCurrentClassLogger();
 
-                var fs = new FileSystem();
-                var scriptCsPreProcessor = new FilePreProcessor(fs, LogManager.GetCurrentClassLogger(), new ILineProcessor[] {
-                        new LoadLineProcessor(fs),
-                        new UsingLineProcessor(),
-                        new ReferenceLineProcessor(fs)
-                    });
+                //var initializationServices = new InitializationServices(scriptcsLogger);
+                //initializationServices.GetAppDomainAssemblyResolver().Initialize();
 
-                var processResult = scriptCsPreProcessor.ProcessFile(csxPath);
+                var scriptServicesBuilder = new ScriptServicesBuilder(new ScriptConsole(), scriptcsLogger).
+                    LogLevel(LogLevel.Info).Cache(false).Repl(false).ScriptName(csxPath).ScriptEngine<NullScriptEngine>();
+
+                                _scriptServices = scriptServicesBuilder.Build();
+
+
+                var runtimeField = typeof(ScriptServicesBuilder).GetField("_runtimeServices", BindingFlags.NonPublic |
+                                              BindingFlags.Instance);
+                var autofacContainer = ((RuntimeServices)runtimeField.GetValue(scriptServicesBuilder)).Container;
 
                 var mscorlib = MetadataReference.CreateFromAssembly(typeof(object).GetTypeInfo().Assembly);
                 var systemCore = MetadataReference.CreateFromAssembly(typeof(Enumerable).GetTypeInfo().Assembly);
                 var scriptcsContracts = MetadataReference.CreateFromAssembly(typeof(IScriptHost).Assembly);
                 var references = new List<MetadataReference> { mscorlib, systemCore, scriptcsContracts };
+                var usings = new List<string>();
 
-                var baseAssemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
-                foreach (var importedReference in processResult.References)
-                {
-                    if (fs.FileExists(importedReference))
-                    {
-                        references.Add(MetadataReference.CreateFromFile(importedReference));
-                    }
-                    else
-                    {
-                        references.Add(MetadataReference.CreateFromFile(Path.Combine(baseAssemblyPath, importedReference.ToLower().EndsWith(".dll") ? importedReference : importedReference + ".dll")));
-                    }
-                }
+                var processResult = _scriptServices.FilePreProcessor.ProcessFile(csxPath);
+
+                //file usings
+                usings.AddRange(processResult.Namespaces);
+
+                //#r references
+                ImportReferences(references, processResult.References);
+
+                var assemblyPaths = _scriptServices.AssemblyResolver.GetAssemblyPaths(_env.Path);
+                //nuget references
+                ImportReferences(references, assemblyPaths);
+
+                //script packs
+                //var scriptPacks = _scriptServices.ScriptPackResolver.GetPacks().ToList();
+                var scriptPackTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).
+                    Where(t => t.GetInterfaces().Any(i => i.FullName == "ScriptCs.Contracts.IScriptPack")).ToArray();
+
+                usings.AddRange(scriptPackTypes.Select(x => x.Namespace));
+
+                //foreach (var scriptPackType in scriptPackTypes)
+                //{
+                //    try
+                //    {
+                //        scriptPacks.Add(Activator.CreateInstance(scriptPackType) as IScriptPack);
+                //    }
+                //    catch (Exception e)
+                //    {
+                //        scriptcsLogger.Error("Error activiatig script pack", e);
+                //    }
+                //}
+
+                //if (scriptPacks != null && scriptPacks.Any())
+                //{
+                //    var scriptPackSession = new ScriptPackSession(scriptPacks, new string[0]);
+                //    scriptPackSession.InitializePacks();
+
+                //    //script pack references
+                //    ImportReferences(references, scriptPackSession.References);
+
+                //    //script pack usings
+                //    usings.AddRange(scriptPackSession.Namespaces);
+                //}
+
+                var parseOptions = new CSharpParseOptions(LanguageVersion.CSharp6, DocumentationMode.Parse,
+SourceCodeKind.Script);
+                var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, usings: usings.Distinct());
 
                 var projectId = ProjectId.CreateNewId("ScriptCs");
                 var project = ProjectInfo.Create(projectId, VersionStamp.Create(), "ScriptCs", "ScriptCs.dll", LanguageNames.CSharp, null, null,
@@ -86,9 +159,24 @@ namespace OmniSharp.ScriptCs
             }
         }
 
+        private void ImportReferences(List<MetadataReference> listOfReferences, IEnumerable<string> referencesToImport)
+        {
+            foreach (var importedReference in referencesToImport)
+            {
+                if (_scriptServices.FileSystem.IsPathRooted(importedReference))
+                {
+                    if (_scriptServices.FileSystem.FileExists(importedReference))
+                        listOfReferences.Add(MetadataReference.CreateFromFile(importedReference));
+                }
+                else
+                {
+                    listOfReferences.Add(MetadataReference.CreateFromFile(Path.Combine(baseAssemblyPath, importedReference.ToLower().EndsWith(".dll") ? importedReference : importedReference + ".dll")));
+                }
+            }
+        }
+
         private void AddFile(string filePath, ProjectId projectId)
         {
-
             using (var stream = File.OpenRead(filePath))
             using (var reader = new StreamReader(stream))
             {
@@ -100,10 +188,7 @@ namespace OmniSharp.ScriptCs
         .WithSourceCodeKind(SourceCodeKind.Script)
        .WithTextLoader(TextLoader.From(TextAndVersion.Create(SourceText.From(csxFile), VersionStamp.Create())));
                 _workspace.AddDocument(documentInfo);
-
             }
-
-
         }
     }
 }
