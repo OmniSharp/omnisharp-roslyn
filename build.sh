519f6c285bebfda48644abd86596991dcb6d1704
James Humphries <James@James-humphries.co.uk>
2015-02-24 12:35:48 +0000

    more script changes

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd ../../
 kvm use 1.0.0-beta3
 kpm bundle src/OmniSharp --no-source --out artifacts/build/omnisharp --runtime KRE-Mono.1.0.0-beta3 2>&1 | tee buildlog
-# work around for kpm pack returning an exit code 0 on failure 
+# work around for kpm bundle returning an exit code 0 on failure 
 grep "Build succeeded" buildlog
 
 # work around for kpm pack not preserving the executable flag on klr when copied
