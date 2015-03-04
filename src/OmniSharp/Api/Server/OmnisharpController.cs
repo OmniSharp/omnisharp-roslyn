f692c241b593bd0608c7640c3784806737dee249
sriramgd <sriramgd@gmail.com>
2015-03-03 22:45:44 -0500

    Adding stop server support

---
+++ src/OmniSharp/Api/Server/OmnisharpController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Runtime;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OmniSharp.Api.Server
+{
+    public partial class OmnisharpController : Controller
+    {
+        protected readonly IApplicationShutdown _applicationShutdown;
+        public OmnisharpController(IApplicationShutdown applicationShutdown)
+        {
+            _applicationShutdown = applicationShutdown;
+        }
+
+        [HttpPost("/stopserver")]
+        public void StopServer()
+        {
+            _applicationShutdown.RequestShutdown();
+        }
+    }
+}
