090eaa8feea9359a84f68904eaa5d5283fc7c736
Johannes Rieken <jrieken@microsoft.com>
2015-03-12 19:32:55 +0100

    add test for transient files being overriden by project adding file

--- tests/OmniSharp.Tests/UpdateBufferFilterFacts.cs
+++ tests/OmniSharp.Tests/UpdateBufferFilterFacts.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 using OmniSharp.Filters;
 using Xunit;
 
@@ -53,6 +55,7 @@ namespace OmniSharp.Tests
             var bufferFilter = new UpdateBufferFilter(workspace);
             bufferFilter.OnActionExecuting(TestHelpers.CreateActionExecutingContext(new Models.Request() { FileName = "test2.cs", Buffer = "interface I {}" }));
 
+            Assert.Equal(2, workspace.CurrentSolution.GetDocumentIdsWithFilePath("test2.cs").Length);
             var docId = workspace.CurrentSolution.GetDocumentIdsWithFilePath("test2.cs").FirstOrDefault();
             Assert.NotNull(docId);
             var sourceText = await workspace.CurrentSolution.GetDocument(docId).GetTextAsync();
@@ -62,6 +65,40 @@ namespace OmniSharp.Tests
             Assert.NotNull(docId);
             sourceText = await workspace.CurrentSolution.GetDocument(docId).GetTextAsync();
             Assert.Equal("class C {}", sourceText.ToString());
+
+
+            var project1 = workspace.CurrentSolution.Projects.First();
+
+        }
+
+        [Fact]
+        public async Task UpdateBuffer_TransientDocumentsDisappearWhenProjectAddsThem()
+        {
+            var workspace = TestHelpers.CreateSimpleWorkspace(new Dictionary<string, string>
+            {
+                { "test.cs", "class C {}" }
+            });
+            var bufferFilter = new UpdateBufferFilter(workspace);
+            bufferFilter.OnActionExecuting(TestHelpers.CreateActionExecutingContext(new Models.Request() { FileName = "transient.cs", Buffer = "interface I {}" }));
+
+            var docIds = workspace.CurrentSolution.GetDocumentIdsWithFilePath("transient.cs");
+            Assert.Equal(2, docIds.Length);
+
+            // simulate a project system adding the file for real
+            var project1 = workspace.CurrentSolution.Projects.First();
+            var document = DocumentInfo.Create(DocumentId.CreateNewId(project1.Id), "transient.cs",
+                loader: TextLoader.From(TextAndVersion.Create(SourceText.From("enum E{}"), VersionStamp.Create())),
+                filePath: "transient.cs");
+            workspace.CurrentSolution.AddDocument(document);
+
+            docIds = workspace.CurrentSolution.GetDocumentIdsWithFilePath("transient.cs");
+            Assert.Equal(2, docIds.Length);
+
+            bufferFilter.OnActionExecuting(TestHelpers.CreateActionExecutingContext(new Models.Request() { FileName = "transient.cs", Buffer = "enum E {}" }));
+            var sourceText = await workspace.CurrentSolution.GetDocument(docIds.First()).GetTextAsync();
+            Assert.Equal("enum E {}", sourceText.ToString());
+            sourceText = await workspace.CurrentSolution.GetDocument(docIds.Last()).GetTextAsync();
+            Assert.Equal("enum E {}", sourceText.ToString());
         }
     }
 }
