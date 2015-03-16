60a07814aae4d2eac1e91c727984b32f9a107939
Jason Imison <jasonimison@gmail.com>
2015-03-16 20:26:07 +0000

    Modified build script for Appveyor

--- build.cmd
+++ build.cmd
@@ -1,7 +1,24 @@
-@echo off
+rem @echo off
 
+@echo on
+pushd %~dp0
 set "KRE_NUGET_API_URL=https://www.nuget.org/api/v2"
-CALL kvm install 1.0.0-beta3
-CALL kvm use 1.0.0-beta3
-CALL kpm restore
-CALL kpm bundle src/OmniSharp --no-source --out artifacts/build/omnisharp --runtime kre-clr-win-x86.1.0.0-beta3
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/master/kvminstall.ps1'))"
+
+set PATH=%PATH%;%userprofile%\.k\bin
+set KRE_HOME=%USERPROFILE%\.k 
+set
+call kvm install 1.0.0-beta3
+call kvm use 1.0.0-beta3
+call kvm list
+rem set the runtime path because the above commands set \.k<space>\runtimes
+set PATH=%USERPROFILE%\.k\runtimes\kre-clr-win-x86.1.0.0-beta3\bin
+call kpm restore
+cd tests\OmniSharp.Tests
+k test
+cd ..\OmniSharp.Stdio.Tests
+k test
+cd ..\..
+call kpm bundle src\OmniSharp --no-source --out artifacts\build\omnisharp --runtime kre-clr-win-x86.1.0.0-beta3
+
+popd
