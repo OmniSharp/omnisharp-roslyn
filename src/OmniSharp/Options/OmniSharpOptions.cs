97501f4f009162ba2a99eeecb4d809a3c4f7f27d
David Fowler <davidfowl@gmail.com>
2015-04-30 03:07:34 -0700

    Allow specifying the visual studio version and toolset version for msbuild based projects

--- src/OmniSharp/Options/OmniSharpOptions.cs
+++ src/OmniSharp/Options/OmniSharpOptions.cs
@@ -4,11 +4,14 @@
     {
         public AspNet5Options AspNet5 { get; set; }
         
+        public MSBuildOptions MsBuild { get; set; }
+
         public FormattingOptions FormattingOptions { get; set; }
         
         public OmniSharpOptions()
         {
             AspNet5 = new AspNet5Options();
+            MsBuild = new MSBuildOptions();
             FormattingOptions = new FormattingOptions();
         }
     }
