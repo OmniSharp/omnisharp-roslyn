3a233d56690186d0c2ed0c9b9e3e97930e93ff38
Rafael Teixeira <monoman@gmail.com>
2015-05-05 17:02:04 -0300

    Publish works - but tests still bork

--- tests/OmniSharp.Tests/Fakes/FakeLoggerFactory.cs
+++ tests/OmniSharp.Tests/Fakes/FakeLoggerFactory.cs
@@ -5,6 +5,33 @@ namespace OmniSharp.Tests
 {
     public class FakeLoggerFactory : ILoggerFactory
     {
+        private class NullLogger : ILogger, IDisposable
+        {
+            private NullLogger()
+            {
+            }
+
+            public readonly static ILogger Instance = new NullLogger();
+
+            public IDisposable BeginScope(object state)
+            {
+                return this;
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return false;
+            }
+
+            public void Log(LogLevel logLevel, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
+            {
+            }
+
+            void IDisposable.Dispose()
+            {
+            }
+        }
+
         public LogLevel MinimumLevel { get; set; }
 
         public void AddProvider(ILoggerProvider provider)
