380c8725eb10ba2c3f8aa48cd541dd0db6bfa405
filipw <wojcieszyn@gmail.com>
2015-05-07 14:42:15 +0200

    added GoToFile action

--- src/OmniSharp/Api/Navigation/OmnisharpController.GotoDefinition.cs
+++ src/OmniSharp/Api/Navigation/OmnisharpController.GotoDefinition.cs
@@ -10,6 +10,18 @@ namespace OmniSharp
 {
     public partial class OmnisharpController
     {
+        [HttpPost("gotofile")]
+        public async Task<QuickFixResponse> GoToFile(Request request)
+        {
+            var docs = _workspace.CurrentSolution.Projects.SelectMany(project => project.Documents).
+                Select(x => new QuickFix { FileName = x.FilePath, Text = x.Name, Line = 1, Column = 1});
+                
+            return new QuickFixResponse(docs);
+        }
+    }
+    
+    public partial class OmnisharpController
+    {
         [HttpPost("gotodefinition")]
         public async Task<IActionResult> GotoDefinition(Request request)
         {
