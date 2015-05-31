a9ae8380088efd87a8f3528b7b55ec73c36fa076
Jason Imison <jasonimison@gmail.com>
2015-05-31 04:40:42 +0100

    magic

--- src/OmniSharp/Services/NRefactoryCodeActionProvider.cs
+++ src/OmniSharp/Services/NRefactoryCodeActionProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using ICSharpCode.NRefactory6.CSharp.Refactoring;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 
@@ -12,9 +13,9 @@ namespace OmniSharp.Services
         public IEnumerable<CodeRefactoringProvider> GetProviders()
         {
             // todo , replace this with the nrefactory filtered list of providers
-            var types = typeof(UseVarKeywordAction)
-                                .Assembly
-                                .GetExportedTypes()
+            
+            var types = Assembly.Load("Microsoft.CodeAnalysis.CSharp.Features, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")
+                                .GetTypes()
                                 .Where(t => typeof(CodeRefactoringProvider).IsAssignableFrom(t));
 
             IEnumerable<CodeRefactoringProvider> providers =
