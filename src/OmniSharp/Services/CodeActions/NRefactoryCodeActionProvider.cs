93edd2fb54341aa7c5cd4a81ba024fcadb01af2e
nosami <jasonimison@gmail.com>
2015-06-02 04:20:25 +0100

    nr6 alpha3 fixes aren't so great

--- src/OmniSharp/Services/CodeActions/NRefactoryCodeActionProvider.cs
+++ src/OmniSharp/Services/CodeActions/NRefactoryCodeActionProvider.cs
@@ -1,8 +1,10 @@
 #if DNX451
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Reflection;
 using ICSharpCode.NRefactory6.CSharp.Refactoring;
+using Microsoft.CodeAnalysis.CodeFixes;
 
 namespace OmniSharp.Services
 {
@@ -11,6 +13,8 @@ namespace OmniSharp.Services
         public NRefactoryCodeActionProvider() : base(typeof(UseVarKeywordAction).Assembly)
         {
         }
+
+        public override IEnumerable<CodeFixProvider> CodeFixes => Enumerable.Empty<CodeFixProvider>();
 
         public override string ProviderName => "NRefactory";
 
