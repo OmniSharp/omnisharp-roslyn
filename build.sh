a01bbc8e9be42389eac350bafa6c7122c429dce2
nosami <jasonimison@gmail.com>
2015-06-10 18:02:26 +0100

    attempt to use beta6 dnu

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 rm -rf artifacts
-if ! type kvm > /dev/null 2>&1; then
+if ! type dnvm > /dev/null 2>&1; then
     curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
 fi
+
+# HACK - dnu restore with beta4 fails most of the time
+# due to timeouts or other failures.
+# Fetch the latest dnu and use that instead
+#export DNX_UNSTABLE_FEED=https://www.myget.org/F/aspnetmaster/api/v2/
+dnvm update-self
+dnvm upgrade -unstable
+dnu restore
+# end hack
+
 dnvm install 1.0.0-beta4
+dnvm alias default 1.0.0-beta4
 dnu restore
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd tests/OmniSharp.Tests
@@ -15,6 +26,6 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd ../../
 dnvm use 1.0.0-beta4
 dnu publish src/OmniSharp --no-source --out artifacts/build/omnisharp --runtime dnx-mono.1.0.0-beta4 2>&1 | tee buildlog
-# work around for kpm bundle returning an exit code 0 on failure 
+# work around for kpm bundle returning an exit code 0 on failure
 grep "Build failed" buildlog
 rc=$?; if [[ $rc == 0 ]]; then exit 1; fi
