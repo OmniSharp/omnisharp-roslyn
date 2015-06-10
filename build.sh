8eb0a797bb9b77ac7a4c6ad054614bafa5840ee6
nosami <jasonimison@gmail.com>
2015-06-10 22:25:05 +0100

    only push packages if the script is being ran on Travis and this isn't a PR

--- build.sh
+++ build.sh
@@ -26,10 +26,13 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd ../../
 dnvm use 1.0.0-beta4
 dnu publish src/OmniSharp --configuration Release --no-source --out artifacts/build/omnisharp --runtime dnx-mono.1.0.0-beta4 2>&1 | tee buildlog
-OMNISHARP_VERSION=1.0.0
-wget https://www.nuget.org/nuget.exe -O /tmp/nuget.exe
-mono /tmp/nuget.exe push artifacts/build/omnisharp/approot/packages/OmniSharp/$OMNISHARP_VERSION/OmniSharp.$OMNISHARP_VERSION.nupkg $MYGET_AUTH -Source https://www.myget.org/F/omnisharp/api/v2/package
-mono /tmp/nuget.exe push artifacts/build/omnisharp/approot/packages/OmniSharp.Stdio/$OMNISHARP_VERSION/OmniSharp.Stdio.$OMNISHARP_VERSION.nupkg $MYGET_AUTH -Source https://www.myget.org/F/omnisharp/api/v2/package
+export
+if ["$TRAVIS" = true] && ["$TRAVIS_PULL_REQUEST" = false];  then
+    OMNISHARP_VERSION=1.0.0
+    wget https://www.nuget.org/nuget.exe -O /tmp/nuget.exe
+    mono /tmp/nuget.exe push artifacts/build/omnisharp/approot/packages/OmniSharp/$OMNISHARP_VERSION/OmniSharp.$OMNISHARP_VERSION.nupkg $MYGET_AUTH -Source https://www.myget.org/F/omnisharp/api/v2/package
+    mono /tmp/nuget.exe push artifacts/build/omnisharp/approot/packages/OmniSharp.Stdio/$OMNISHARP_VERSION/OmniSharp.Stdio.$OMNISHARP_VERSION.nupkg $MYGET_AUTH -Source https://www.myget.org/F/omnisharp/api/v2/package
+fi
 # work around for kpm bundle returning an exit code 0 on failure
 grep "Build failed" buildlog
 rc=$?; if [[ $rc == 0 ]]; then exit 1; fi
