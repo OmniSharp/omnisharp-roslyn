55af98a8c6901d3d289a13677ebbe3d487f21485
Andrew Benz <andorbal@gmail.com>
2015-06-21 14:39:55 -0500

    Add initial ability to generate metadata source from a symbol

--- src/OmniSharp/Api/v1/Navigation/OmnisharpController.GotoDefinition.cs
+++ src/OmniSharp/Api/v1/Navigation/OmnisharpController.GotoDefinition.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.FindSymbols;
 using Microsoft.CodeAnalysis.Text;
 using OmniSharp.Models;
@@ -27,17 +31,54 @@ namespace OmniSharp
                 
                 if (symbol != null)
                 {
-                    var lineSpan = symbol.Locations.First().GetMappedLineSpan();
-                    response = new GotoDefinitionResponse
+                    var location = symbol.Locations.First();
+
+                    if (location.IsInSource)
                     {
-                        FileName = lineSpan.Path,
-                        Line = lineSpan.StartLinePosition.Line + 1,
-                        Column = lineSpan.StartLinePosition.Character + 1
-                    };
+                        var lineSpan = symbol.Locations.First().GetMappedLineSpan();
+                        response = new GotoDefinitionResponse
+                        {
+                            FileName = lineSpan.Path,
+                            Line = lineSpan.StartLinePosition.Line + 1,
+                            Column = lineSpan.StartLinePosition.Character + 1
+                        };
+                    }
+#if DNX451
+                    else if (location.IsInMetadata)
+                    {
+                        var temporaryDocument = document.Project.AddDocument("foo", string.Empty);
+                        var topLevelSymbol = GetTopLevelContainingNamedType(symbol);
+
+                        var assembly = Assembly.Load("Microsoft.CodeAnalysis.CSharp.Features, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+                        var type = assembly.GetType("Microsoft.CodeAnalysis.CSharp.MetadataAsSource.CSharpMetadataAsSourceService");
+                        
+                        object service = Activator.CreateInstance(type, new object[] { temporaryDocument.Project.LanguageServices });
+                        var method = type.GetMethod("AddSourceToAsync");
+                        
+                        var result = await (Task<Document>)method.Invoke(service, new object[] { temporaryDocument, topLevelSymbol, new CancellationToken() });
+                        var source = await result.GetTextAsync();
+                        response.MetadataSource = source.ToString();
+
+                        //TODO: Find the location of the symbol in the source
+                    }
+#endif
                 }
             }
 
             return new ObjectResult(response);
+        }
+
+        public static INamedTypeSymbol GetTopLevelContainingNamedType(ISymbol symbol)
+        {
+            // Traverse up until we find a named type that is parented by the namespace
+            var topLevelNamedType = symbol;
+            while (topLevelNamedType.ContainingSymbol != symbol.ContainingNamespace ||
+                topLevelNamedType.Kind != SymbolKind.NamedType)
+            {
+                topLevelNamedType = topLevelNamedType.ContainingSymbol;
+            }
+
+            return (INamedTypeSymbol)topLevelNamedType;
         }
     }
 }
