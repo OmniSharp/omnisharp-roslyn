a8a6d480afa01d7dde9a6abc0cdd63f14c3fcea3
nosami <jasonimison@gmail.com>
2015-07-07 23:33:19 +0100

    updated build script to beta5

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ dnvm upgrade -unstable
 dnu restore
 # end hack
 
-dnvm install 1.0.0-beta4
-dnvm alias default 1.0.0-beta4
+dnvm install 1.0.0-beta5
+dnvm alias default 1.0.0-beta5
 dnu restore
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd tests/OmniSharp.Tests
@@ -24,22 +24,22 @@ cd ../OmniSharp.Stdio.Tests
 dnx . test -parallel none
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd ../../
-dnvm use 1.0.0-beta4
-dnu publish src/OmniSharp --configuration Release --no-source --out artifacts/build/omnisharp --runtime dnx-mono.1.0.0-beta4 2>&1 | tee buildlog
+dnvm use 1.0.0-beta5
+dnu publish src/OmniSharp --configuration Release --no-source --out artifacts/build/omnisharp --runtime dnx-mono.1.0.0-beta5 2>&1 | tee buildlog
 # work around for kpm bundle returning an exit code 0 on failure
 grep "Build failed" buildlog
 rc=$?; if [[ $rc == 0 ]]; then exit 1; fi
 
 curl -LO http://nuget.org/nuget.exe
-mono nuget.exe install dnx-clr-win-x86 -Version 1.0.0-beta4 -Prerelease -OutputDirectory artifacts/build/omnisharp/approot/packages
+mono nuget.exe install dnx-clr-win-x86 -Version 1.0.0-beta5 -Prerelease -OutputDirectory artifacts/build/omnisharp/approot/packages
 
-if [ ! -d "artifacts/build/omnisharp/approot/packages/dnx-clr-win-x86.1.0.0-beta4" ]; then
-    echo 'ERROR: Can not find dnx-clr-win-x86.1.0.0-beta4 in output exiting!'
+if [ ! -d "artifacts/build/omnisharp/approot/packages/dnx-clr-win-x86.1.0.0-beta5" ]; then
+    echo 'ERROR: Can not find dnx-clr-win-x86.1.0.0-beta5 in output exiting!'
     exit 1
 fi
 
-if [ ! -d "artifacts/build/omnisharp/approot/packages/dnx-mono.1.0.0-beta4" ]; then
-    echo 'ERROR: Can not find dnx-mono.1.0.0-beta4 in output exiting!'
+if [ ! -d "artifacts/build/omnisharp/approot/packages/dnx-mono.1.0.0-beta5" ]; then
+    echo 'ERROR: Can not find dnx-mono.1.0.0-beta5 in output exiting!'
     exit 1
 fi
 
