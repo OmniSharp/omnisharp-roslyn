64c72110b9868b7878279f54132ef6c1d3918369
nosami <jasonimison@gmail.com>
2015-08-27 00:26:10 +0100

    less parameter passing around

--- src/OmniSharp/Api/v1/Refactoring/OmnisharpController.FixUsings.cs
+++ src/OmniSharp/Api/v1/Refactoring/OmnisharpController.FixUsings.cs
@@ -12,13 +12,23 @@ namespace OmniSharp
         public async Task<FixUsingsResponse> FixUsings(FixUsingsRequest request)
         {
             var document = _workspace.GetDocument(request.FileName);
-            var docText = await document.GetTextAsync();
             var response = new FixUsingsResponse();
 
             if (document != null)
             {
-                var semanticModel = await document.GetSemanticModelAsync();
-                response = await FixUsingsWorker.FixUsings(_workspace, request.FileName, document, semanticModel, request.WantsTextChanges);
+                response = await new FixUsingsWorker().FixUsings(_workspace, document);
+                if (!request.WantsTextChanges)
+                {
+                    // return the new document
+                    var docText = await _workspace.CurrentSolution.GetDocument(document.Id).GetTextAsync();
+                    response.Buffer = docText.ToString();
+                }
+                else
+                {
+                    // return the text changes
+                    var changes = await _workspace.CurrentSolution.GetDocument(document.Id).GetTextChangesAsync(document);
+                    response.Changes = await LinePositionSpanTextChange.Convert(document, changes);
+                }
             }
 
             return response;
