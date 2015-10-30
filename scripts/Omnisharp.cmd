337b006831ad28bc109da8a493f77ab3a6c55786
David Driscoll <david.driscoll@gmail.com>
2015-10-30 14:56:50 -0400

    Add Bootstrap to script commands for starting omnisharp up

--- scripts/Omnisharp.cmd
+++ scripts/Omnisharp.cmd
@@ -1,3 +1,5 @@
 SETLOCAL
 
-"%USERPROFILE%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta4\bin\dnx" %~dp0..\src\OmniSharp run %*
+rem make sure we're bootstrapped
+for /f "delims=" %%a in ('%~dp0Bootstrap.cmd') do @set LOCATION=%%a
+"%USERPROFILE%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta4\bin\dnx" %LOCATION% run %*
