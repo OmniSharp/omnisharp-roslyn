db54193e3c9b440b1404dd41898e464b20c65bdd
David Driscoll <david.driscoll@gmail.com>
2015-11-09 15:31:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ echo $thispwd
 
 #pushd "$(dirname "$0")"
 
+echo $0
+thispwd=`pwd`
+echo $thispwd
+
 rm -rf artifacts
 if ! type dnvm > /dev/null 2>&1; then
     curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
