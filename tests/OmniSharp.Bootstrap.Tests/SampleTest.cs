0c67a5b12575253e3693adc5f1426fe5df1e71b0
David Driscoll <david.driscoll@gmail.com>
2015-11-26 22:51:17 -0500

    Added some simple tests

--- tests/OmniSharp.Bootstrap.Tests/SampleTest.cs
+++ tests/OmniSharp.Bootstrap.Tests/SampleTest.cs
@@ -55,12 +55,40 @@ namespace OmniSharp.Bootstrap.Tests
     public class SampleTest
     {
         [Fact]
-        public void Test1()
+        public void AddsPlugins()
         {
             var program = new Program(new FakeApplicationEnvironment());
 
-            program.ParseArguments(new string[] { });
-            Assert.True(false);
+            program.ParseArguments(new string[] { "--plugins", "/a/b/c/d" });
+            Assert.Contains("/a/b/c/d", program.PluginPaths);
+        }
+
+        [Fact]
+        public void AddsAPluginByName()
+        {
+            var program = new Program(new FakeApplicationEnvironment());
+
+            program.ParseArguments(new string[] { "--plugin-name", "PluginA", "--plugin-name", "PluginB" });
+            Assert.True(program.PluginNames.Any(z => z.Key == "PluginA"));
+            Assert.True(program.PluginNames.Any(z => z.Key == "PluginB"));
+        }
+
+        [Fact]
+        public void AddsAPluginByNameWithVersion()
+        {
+            var program = new Program(new FakeApplicationEnvironment());
+
+            program.ParseArguments(new string[] { "--plugin-name", "PluginA@1.0.0", "--plugin-name", "PluginB" });
+            Assert.True(program.PluginNames.Any(z => z.Key == "PluginA" && z.Value == "1.0.0"));
+        }
+
+        [Fact]
+        public void SetsSolutionRoot()
+        {
+            var program = new Program(new FakeApplicationEnvironment());
+
+            program.ParseArguments(new string[] { "-s", "/path/to/project" });
+            Assert.Equal(Path.GetFullPath("/path/to/project"), program.SolutionRoot);
         }
     }
 }
