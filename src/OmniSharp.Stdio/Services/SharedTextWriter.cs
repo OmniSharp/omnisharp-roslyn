c2a54cf7541af7d434573eb5548007b73a71474c
Troy Dai <troy.dai@outlook.com>
2016-01-24 21:25:10 -0800

    Update SharedTextWriter
    Using a BlockedCollection instead of spin off a thread for each write
    action.

--- src/OmniSharp.Stdio/Services/SharedTextWriter.cs
+++ src/OmniSharp.Stdio/Services/SharedTextWriter.cs
@@ -1,21 +1,33 @@
-﻿using System.IO;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OmniSharp.Stdio.Services
 {
     public class SharedTextWriter : ISharedTextWriter
     {
+        private BlockingCollection<object> _queue = new BlockingCollection<object>();
+
         private readonly object _lock = new object();
         private readonly TextWriter _writer;
 
         public SharedTextWriter(TextWriter writer)
         {
             _writer = writer;
+
+            var thread = new Thread(() => { while (true) WriteLine(_queue.Take()); })
+            {
+                Name = $"{nameof(SharedTextWriter)} {nameof(BlockingCollection<object>)}",
+                IsBackground = true
+            };
+
+            thread.Start();
         }
 
         public void WriteLine(object value)
         {
-            lock(_lock)
+            lock (_lock)
             {
                 _writer.WriteLine(value);
             }
@@ -23,7 +35,9 @@ namespace OmniSharp.Stdio.Services
 
         public Task WriteLineAsync(object value)
         {
-            return Task.Factory.StartNew(() => WriteLine(value));
+            _queue.Add(value);
+
+            return Task.FromResult(0);
         }
     }
 }
