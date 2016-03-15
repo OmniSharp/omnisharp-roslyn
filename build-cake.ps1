d7a5a67c344f1a52683e4bf460724adb526d3056
hal-ler <hal_ler@yahoo.com>
2016-03-15 01:56:18 +0100

    Update lost in rebase.

--- build-cake.ps1
+++ build-cake.ps1
@@ -1,2 +1,5 @@
+# Hack until dotnet/cli##1701 is accepted
+$env:DOTNET_INSTALL_DIR=$PWD.Path+"\.dotnet"
+
 Invoke-WebRequest http://cakebuild.net/bootstrapper/windows -OutFile cake-bootstrap.ps1
-.\cake-bootstrap.ps1 @args
+.\cake-bootstrap.ps1 --experimental @args
