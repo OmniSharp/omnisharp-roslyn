ef6b6de7dcf7a2adce8517852e82858fc3fe9b41
hal-ler <hal_ler@yahoo.com>
2016-04-04 21:55:24 +0200

    Roslyn 1.3.0

--- src/OmniSharp.Roslyn.CSharp/Services/Navigation/FindImplementationsService.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/Navigation/FindImplementationsService.cs
@@ -33,7 +33,7 @@ namespace OmniSharp.Roslyn.CSharp.Services.Navigation
                 var semanticModel = await document.GetSemanticModelAsync();
                 var sourceText = await document.GetTextAsync();
                 var position = sourceText.Lines.GetPosition(new LinePosition(request.Line - 1, request.Column - 1));
-                var symbol = SymbolFinder.FindSymbolAtPosition(semanticModel, position, _workspace);
+                var symbol = await SymbolFinder.FindSymbolAtPositionAsync(semanticModel, position, _workspace);
                 var quickFixes = new List<QuickFix>();
 
                 var implementations = await SymbolFinder.FindImplementationsAsync(symbol, _workspace.CurrentSolution);
