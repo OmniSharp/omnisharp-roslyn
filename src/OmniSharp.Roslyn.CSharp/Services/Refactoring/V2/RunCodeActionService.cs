e5801c570a5b8cc60da79c85573a369e6ba02a77
David Driscoll <david.driscoll@gmail.com>
2016-06-06 08:29:53 -0400

    do not apply changes for code actions through the workspace.  Instead simply return the changes for the editor to handle.

--- src/OmniSharp.Roslyn.CSharp/Services/Refactoring/V2/RunCodeActionService.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/Refactoring/V2/RunCodeActionService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.Extensions.Logging;
 using OmniSharp.Mef;
 using OmniSharp.Models.V2;
@@ -43,16 +44,17 @@ namespace OmniSharp.Roslyn.CSharp.Services.Refactoring.V2
             var operations = await action.GetOperationsAsync(CancellationToken.None);
 
             var solution = _workspace.CurrentSolution;
+            IEnumerable<OmniSharp.Models.ModifiedFileResponse> changes = Enumerable.Empty<OmniSharp.Models.ModifiedFileResponse>();
+            var directoryName = Path.GetDirectoryName(request.FileName);
             foreach (var o in operations)
             {
-                o.Apply(_workspace, CancellationToken.None);
+                var applyChangesOperation = o as ApplyChangesOperation;
+                if (applyChangesOperation != null) {
+                    changes = changes.Concat(await FileChanges.GetFileChangesAsync(applyChangesOperation.ChangedSolution, _workspace.CurrentSolution, directoryName, request.WantsTextChanges));
+                }
             }
 
-            var directoryName = Path.GetDirectoryName(request.FileName);
-            var changes = await FileChanges.GetFileChangesAsync(_workspace.CurrentSolution, solution, directoryName, request.WantsTextChanges);
-
             response.Changes = changes;
-            _workspace.TryApplyChanges(_workspace.CurrentSolution);
             return response;
         }
     }
