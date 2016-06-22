c27a0cbb6fdda17cf9c49acc56a724b120fd9528
Troy Dai <troy.dai@outlook.com>
2016-06-22 14:48:40 -0700

    Fix incorrect project path resolver

--- src/OmniSharp.DotNetTest/Helpers/ProjectPathResolver.cs
+++ src/OmniSharp.DotNetTest/Helpers/ProjectPathResolver.cs
@@ -10,7 +10,7 @@ namespace OmniSharp.DotNetTest.Helpers
             var projectFolder = Path.GetDirectoryName(filepath);
             while (!File.Exists(Path.Combine(projectFolder, "project.json")))
             {
-                var parent = Path.GetDirectoryName(filepath);
+                var parent = Path.GetDirectoryName(projectFolder);
                 if (parent == projectFolder)
                 {
                     break;
