12eaf475ae28dc9062acfe5a8e20bfd710687126
Dustin Campbell <dustin@teamcampbell.org>
2016-09-17 15:43:59 -0700

    Update to Roslyn 2.0.0-beta3

--- src/OmniSharp.Abstractions/Configuration.cs
+++ src/OmniSharp.Abstractions/Configuration.cs
@@ -4,7 +4,7 @@ namespace OmniSharp
     {
         public static bool ZeroBasedIndices = false;
 
-        public const string RoslynVersion = "1.3.0.0";
+        public const string RoslynVersion = "2.0.0.0";
         public const string RoslynPublicKeyToken = "31bf3856ad364e35";
 
         public static string GetRoslynAssemblyFullName(string name)
