4ca7f7f9a97aa589e0f0f402e513027e61ad130a
Dustin Campbell <dustin@teamcampbell.org>
2016-09-21 07:24:55 -0700

    Allow project to specify MSBuildExtensionsPath

--- src/OmniSharp.MSBuild/Options/MSBuildOptions.cs
+++ src/OmniSharp.MSBuild/Options/MSBuildOptions.cs
@@ -5,6 +5,7 @@ namespace OmniSharp.Options
         public string ToolsVersion { get; set; }
         public string VisualStudioVersion { get; set; }
         public bool WaitForDebugger { get; set; }
+        public string MSBuildExtensionsPath { get; set; }
 
         // TODO: Allow loose properties
         // public IConfiguration Properties { get; set; }
