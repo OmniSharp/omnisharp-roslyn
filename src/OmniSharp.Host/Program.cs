fa001f1f5bf21490d80c78bc1485516fca1e8278
filipw <wojcieszyn@gmail.com>
2016-12-20 08:53:16 +0100

    allow loglevel to passed as argument

--- src/OmniSharp.Host/Program.cs
+++ src/OmniSharp.Host/Program.cs
@@ -48,6 +48,15 @@ namespace OmniSharp
                     enumerator.MoveNext();
                     serverPort = int.Parse((string)enumerator.Current);
                 }
+                else if (arg.ToLowerInvariant() == "-loglevel")
+                {
+                    enumerator.MoveNext();
+                    LogLevel level;
+                    if (Enum.TryParse((string) enumerator.Current, true, out level))
+                    {
+                        logLevel = level;
+                    }
+                }
                 else if (arg == "-v")
                 {
                     logLevel = LogLevel.Debug;
