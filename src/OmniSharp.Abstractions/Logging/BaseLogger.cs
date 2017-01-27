eb4494c5469c305bfe74a2bf704915ad19b219a8
Dustin Campbell <dustin@teamcampbell.org>
2017-01-27 09:46:50 -0800

    Introduce BaseLogger class

---
+++ src/OmniSharp.Abstractions/Logging/BaseLogger.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace OmniSharp.Logging
+{
+    public abstract class BaseLogger : ILogger
+    {
+        private readonly string _categoryName;
+
+        private static readonly string s_padding = new string(' ', 8);
+        private static readonly string s_newLinePlusPadding = Environment.NewLine + s_padding;
+
+        [ThreadStatic]
+        private static StringBuilder g_builder;
+
+        protected BaseLogger(string categoryName)
+        {
+            this._categoryName = categoryName;
+        }
+
+        protected abstract void WriteMessage(string message);
+
+        private StringBuilder GetBuilder()
+        {
+            var builder = g_builder;
+            g_builder = null;
+
+            if (builder == null)
+            {
+                builder = new StringBuilder();
+            }
+
+            return builder;
+        }
+
+        private void ReleaseBuilder(StringBuilder builder)
+        {
+            builder.Clear();
+            if (builder.Capacity > 1024)
+            {
+                builder.Capacity = 1024;
+            }
+
+            g_builder = builder;
+        }
+
+        private string CreateMessage(LogLevel logLevel, string messageText, Exception exception)
+        {
+            var builder = GetBuilder();
+            try
+            {
+                if (!string.IsNullOrEmpty(messageText))
+                {
+                    builder.Append(GetLogLevelPrefix(logLevel));
+                    builder.Append(_categoryName);
+                    builder.AppendLine();
+
+                    builder.Append(s_padding);
+                    var length = builder.Length;
+                    builder.AppendLine(messageText);
+                    builder.Replace(Environment.NewLine, s_newLinePlusPadding, length, messageText.Length);
+                }
+
+                if (exception != null)
+                {
+                    builder.AppendLine(exception.ToString());
+                }
+
+                return builder.ToString();
+            }
+            finally
+            {
+                ReleaseBuilder(builder);
+            }
+        }
+
+        private static string GetLogLevelPrefix(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                    return "[trce]: ";
+                case LogLevel.Debug:
+                    return "[dbug]: ";
+                case LogLevel.Information:
+                    return "[info]: ";
+                case LogLevel.Warning:
+                    return "[warn]: ";
+                case LogLevel.Error:
+                    return "[fail]: ";
+                case LogLevel.Critical:
+                    return "[crit]: ";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(LogLevel));
+            }
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var messageText = formatter(state, exception);
+            if (!string.IsNullOrEmpty(messageText) || exception != null)
+            {
+                var message = CreateMessage(logLevel, messageText, exception);
+                WriteMessage(message);
+            }
+        }
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public IDisposable BeginScope<TState>(TState state) => new NoopDisposable();
+
+        private class NoopDisposable : IDisposable
+        {
+            public void Dispose() { }
+        }
+    }
+}
--- tests/TestUtility/Logging/TestLogger.cs
+++ tests/TestUtility/Logging/TestLogger.cs
@@ -1,117 +1,21 @@
-﻿using System;
-using System.Text;
-using Microsoft.Extensions.Logging;
+﻿using OmniSharp.Logging;
 using Xunit.Abstractions;
 
 namespace TestUtility.Logging
 {
-    public class TestLogger : ILogger
+    public class TestLogger : BaseLogger
     {
         private readonly ITestOutputHelper _output;
-        private readonly string _categoryName;
-
-        private static readonly string s_padding = new string(' ', 8);
-        private static readonly string s_newLinePlusPadding = Environment.NewLine + s_padding;
-
-        [ThreadStatic]
-        private static StringBuilder g_builder;
 
         public TestLogger(ITestOutputHelper output, string categoryName)
+            : base(categoryName)
         {
             this._output = output;
-            this._categoryName = categoryName;
         }
 
-        private StringBuilder GetBuilder()
+        protected override void WriteMessage(string message)
         {
-            var builder = g_builder;
-            g_builder = null;
-
-            if (builder == null)
-            {
-                builder = new StringBuilder();
-            }
-
-            return builder;
-        }
-
-        private void ReleaseBuilder(StringBuilder builder)
-        {
-            builder.Clear();
-            if (builder.Capacity > 1024)
-            {
-                builder.Capacity = 1024;
-            }
-
-            g_builder = builder;
-        }
-
-        private void WriteMessage(LogLevel logLevel, string message, Exception exception)
-        {
-            var builder = GetBuilder();
-
-            if (!string.IsNullOrEmpty(message))
-            {
-                builder.Append(GetLogLevelPrefix(logLevel));
-                builder.Append(_categoryName);
-                builder.AppendLine();
-
-                builder.Append(s_padding);
-                var length = builder.Length;
-                builder.AppendLine(message);
-                builder.Replace(Environment.NewLine, s_newLinePlusPadding, length, message.Length);
-            }
-
-            if (exception != null)
-            {
-                builder.AppendLine(exception.ToString());
-            }
-
-            if (builder.Length > 0)
-            {
-                _output.WriteLine(builder.ToString());
-            }
-
-            ReleaseBuilder(builder);
-        }
-
-        private static string GetLogLevelPrefix(LogLevel logLevel)
-        {
-            switch (logLevel)
-            {
-                case LogLevel.Trace:
-                    return "[trce]: ";
-                case LogLevel.Debug:
-                    return "[dbug]: ";
-                case LogLevel.Information:
-                    return "[info]: ";
-                case LogLevel.Warning:
-                    return "[warn]: ";
-                case LogLevel.Error:
-                    return "[fail]: ";
-                case LogLevel.Critical:
-                    return "[crit]: ";
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(LogLevel));
-            }
-        }
-
-        public bool IsEnabled(LogLevel level) => true;
-
-        public IDisposable BeginScope<TState>(TState state) => new NoopDisposable();
-
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-        {
-            var message = formatter(state, exception);
-            if (!string.IsNullOrEmpty(message) || exception != null)
-            {
-                WriteMessage(logLevel, message, exception);
-            }
-        }
-
-        private class NoopDisposable : IDisposable
-        {
-            public void Dispose() { }
+            _output.WriteLine(message);
         }
     }
 }
