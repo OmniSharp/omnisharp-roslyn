66b77f21fbd710f96c5ed60cb9ef230bd3012817
Dustin Campbell <dustin@teamcampbell.org>
2017-02-22 09:17:04 -0800

    Add script to allow launching Mono msbuild to compile OmniSharp

---
+++ msbuild.sh
@@ -0,0 +1,5 @@
+export MSBuildExtensionsPath=~/projects/omnisharp-roslyn/.dotnet/sdk/1.0.0-rc4-004842/
+export CscToolExe=~/projects/omnisharp-roslyn/.dotnet/sdk/1.0.0-rc4-004842/Roslyn/RunCsc.sh
+export MSBuildSDKsPath=~/projects/omnisharp-roslyn/.dotnet/sdk/1.0.0-rc4-004842/Sdks
+
+msbuild "$@"
