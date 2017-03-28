7a3b53a1410c005db53b888f82845d3f5a1277c7
Dustin Campbell <dustin@teamcampbell.org>
2017-03-28 09:58:13 -0700

    Add a couple more legacy xUnit tests and retry logic for test clean up

--- tests/OmniSharp.DotNetTest.Tests/LegacyRunTestFacts.cs
+++ tests/OmniSharp.DotNetTest.Tests/LegacyRunTestFacts.cs
@@ -45,5 +45,53 @@ namespace OmniSharp.DotNetTest.Tests
                 Assert.True(response.Pass);
             }
         }
+
+        [Fact]
+        public async Task RunXunitTheoryWithInlineData1()
+        {
+            using (var testProject = await TestAssets.Instance.GetTestProjectAsync("LegacyXunitTestProject"))
+            using (var host = CreateOmniSharpHost(testProject.Directory, useLegacyDotNetCli: true))
+            {
+                var dotNetCli = host.GetExport<DotNetCliService>();
+                await dotNetCli.RestoreAsync(testProject.Directory, "--infer-runtimes");
+
+                var service = GetRequestHandler(host);
+
+                var request = new RunDotNetTestRequest
+                {
+                    FileName = Path.Combine(testProject.Directory, "TestProgram.cs"),
+                    MethodName = "Main.Test.MainTest.DataDrivenTest1",
+                    TestFrameworkName = "xunit"
+                };
+
+                var response = await service.Handle(request);
+
+                Assert.False(response.Pass);
+            }
+        }
+
+        [Fact]
+        public async Task RunXunitTheoryWithInlineData2()
+        {
+            using (var testProject = await TestAssets.Instance.GetTestProjectAsync("LegacyXunitTestProject"))
+            using (var host = CreateOmniSharpHost(testProject.Directory, useLegacyDotNetCli: true))
+            {
+                var dotNetCli = host.GetExport<DotNetCliService>();
+                await dotNetCli.RestoreAsync(testProject.Directory, "--infer-runtimes");
+
+                var service = GetRequestHandler(host);
+
+                var request = new RunDotNetTestRequest
+                {
+                    FileName = Path.Combine(testProject.Directory, "TestProgram.cs"),
+                    MethodName = "Main.Test.MainTest.DataDrivenTest2",
+                    TestFrameworkName = "xunit"
+                };
+
+                var response = await service.Handle(request);
+
+                Assert.True(response.Pass);
+            }
+        }
     }
 }
