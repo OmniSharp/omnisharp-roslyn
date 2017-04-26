349de1002fd9fcd364fc1927762ed282560eed01
Dustin Campbell <dustin@teamcampbell.org>
2017-04-26 12:36:24 -0700

    Ensure test actually fails

--- tests/OmniSharp.DotNetTest.Tests/AbstractRunTestFacts.cs
+++ tests/OmniSharp.DotNetTest.Tests/AbstractRunTestFacts.cs
@@ -27,7 +27,7 @@ namespace OmniSharp.DotNetTest.Tests
 
         public abstract bool UseLegacyDotNetCli { get; }
 
-        protected async Task RunDotNetTestAsync(string projectName, string methodName, string testFramework, bool shouldPass)
+        protected async Task<RunTestResponse> RunDotNetTestAsync(string projectName, string methodName, string testFramework, bool shouldPass, bool expectResults = true)
         {
             using (var testProject = await TestAssets.Instance.GetTestProjectAsync(projectName))
             using (var host = CreateOmniSharpHost(testProject.Directory, useLegacyDotNetCli: UseLegacyDotNetCli))
@@ -43,6 +43,11 @@ namespace OmniSharp.DotNetTest.Tests
 
                 var response = await service.Handle(request);
 
+                if (expectResults)
+                {
+                    Assert.True(response.Results?.Length > 0, "Expected test to return results.");
+                }
+
                 if (shouldPass)
                 {
                     Assert.True(response.Pass, "Expected test to pass but it failed");
@@ -51,6 +56,8 @@ namespace OmniSharp.DotNetTest.Tests
                 {
                     Assert.False(response.Pass, "Expected test to fail but it passed");
                 }
+
+                return response;
             }
         }
     }
