73c78c49dfc552de50d0572494e39e83a6e75c9e
filipw <wojcieszyn@gmail.com>
2017-04-30 12:43:23 +0200

    clean up of commented code, comments and logic

--- src/OmniSharp.Roslyn.CSharp/Services/Intellisense/IntellisenseService.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/Intellisense/IntellisenseService.cs
@@ -14,6 +14,21 @@ using OmniSharp.Roslyn.CSharp.Services.Documentation;
 
 namespace OmniSharp.Roslyn.CSharp.Services.Intellisense
 {
+    internal static class CompletionItemExtensions
+    {
+        public static IEnumerable<ISymbol> GetCompletionSymbols(this CompletionItem completionItem, IEnumerable<ISymbol> recommendedSymbols)
+        {
+            // for SymbolCompletionProvider, use the logic of extracting information from recommended symbols
+            if (completionItem.Properties.ContainsKey("Provider") && completionItem.Properties["Provider"] == "Microsoft.CodeAnalysis.CSharp.Completion.Providers.SymbolCompletionProvider")
+            {
+                var symbols = recommendedSymbols.Where(x => x.Name == completionItem.Properties["SymbolName"] && (int)x.Kind == int.Parse(completionItem.Properties["SymbolKind"])).Distinct();
+                return symbols ?? Enumerable.Empty<ISymbol>();
+            }
+
+            return Enumerable.Empty<ISymbol>();
+        }
+    }
+
     [OmniSharpHandler(OmniSharpEndpoints.AutoComplete, LanguageNames.CSharp)]
     public class IntellisenseService : IRequestHandler<AutoCompleteRequest, IEnumerable<AutoCompleteResponse>>
     {
@@ -37,98 +52,61 @@ namespace OmniSharp.Roslyn.CSharp.Services.Intellisense
             {
                 var sourceText = await document.GetTextAsync();
                 var position = sourceText.Lines.GetPosition(new LinePosition(request.Line, request.Column));
-
-                //var options = await document.GetOptionsAsync();
-                //options.WithChangedOption(new PerLanguageOption<SnippetsRule>("CompletionOptions", "SnippetsBehavior", SnippetsRule.AlwaysInclude), LanguageNames.CSharp, SnippetsRule.AlwaysInclude);
-                //options.WithChangedOption()
                 var semanticModel = await document.GetSemanticModelAsync();
                 var service = CompletionService.GetService(document);
                 var completionList = await service.GetCompletionsAsync(document, position);
-                //options: options.WithChangedOption(new PerLanguageOption<SnippetsRule>("CompletionOptions", "SnippetsBehavior", SnippetsRule.AlwaysInclude)));
-
-                // Add keywords from the completion list. We'll use the recommender service to get symbols
-                // to create snippets from.
-
-
-                var recommendedSymbols = await Recommender.GetRecommendedSymbolsAtPositionAsync(semanticModel, position, _workspace);
 
                 if (completionList != null)
                 {
+                    // get recommened symbols to match them up later with SymbolCompletionProvider
+                    var recommendedSymbols = await Recommender.GetRecommendedSymbolsAtPositionAsync(semanticModel, position, _workspace);
+
                     foreach (var item in completionList.Items)
                     {
-                        //if (item.Tags.Contains(CompletionTags.Keyword))
+                        var completionText = item.DisplayText;
+                        if (completionText.IsValidCompletionFor(wordToComplete))
                         {
-                            // Note: For keywords, we'll just assume that the completion text is the same
-                            // as the display text.
-                            var keyword = item.DisplayText;
-                            if (keyword.IsValidCompletionFor(wordToComplete))
+                            var symbols = item.GetCompletionSymbols(recommendedSymbols);
+                            if (symbols.Any())
                             {
-                                if (item.Properties.ContainsKey("Provider") && item.Properties["Provider"] == "Microsoft.CodeAnalysis.CSharp.Completion.Providers.SymbolCompletionProvider")
+                                foreach (var symbol in symbols)
                                 {
-                                    //var symbolsTask = GetSymbolsAsync.Invoke(null, new object[] { item, document, default(CancellationToken) }) as Task<ImmutableArray<ISymbol>>;
-                                    //var symbols = await symbolsTask;
-                                    //SymbolFinder.
-                                    //foreach (var symbol in symbols.Where(s => s.Name.IsValidCompletionFor(wordToComplete)))
-                                    //service.GetDescriptionAsync
+                                    if (symbol != null)
                                     {
-                                        var symbols = recommendedSymbols.Where(x => x.Name == item.Properties["SymbolName"] && (int)x.Kind == int.Parse(item.Properties["SymbolKind"])).Distinct();
-
-                                        if (symbols != null && symbols.Any())
+                                        if (request.WantSnippet)
                                         {
-                                            foreach (var symbol in symbols)
+                                            foreach (var completion in MakeSnippetedResponses(request, symbol))
                                             {
-                                                if (symbol != null)
-                                                {
-                                                    if (request.WantSnippet)
-                                                    {
-                                                        foreach (var completion in MakeSnippetedResponses(request, symbol))
-                                                        {
-                                                            completions.Add(completion);
-                                                        }
-                                                    }
-                                                    else
-                                                    {
-                                                        completions.Add(MakeAutoCompleteResponse(request, symbol));
-                                                    }
-                                                }
+                                                completions.Add(completion);
                                             }
-
-                                            continue;
+                                        }
+                                        else
+                                        {
+                                            completions.Add(MakeAutoCompleteResponse(request, symbol));
                                         }
                                     }
                                 }
 
-                                var response = new AutoCompleteResponse()
-                                {
-                                    CompletionText = item.DisplayText,
-                                    DisplayText = item.DisplayText,
-                                    Snippet = item.DisplayText,
-                                    Kind = request.WantKind ? item.Tags.First() : null
-                                };
-
-                                completions.Add(response);
+                                // if we had any symbols from the completion, we can continue, otherwise it means
+                                // the completion didn't have an associated symbol so we'll add it manually
+                                continue;
                             }
+
+                            // for other completions, i.e. keywords, create a simple AutoCompleteResponse
+                            // we'll just assume that the completion text is the same
+                            // as the display text.
+                            var response = new AutoCompleteResponse()
+                            {
+                                CompletionText = item.DisplayText,
+                                DisplayText = item.DisplayText,
+                                Snippet = item.DisplayText,
+                                Kind = request.WantKind ? item.Tags.First() : null
+                            };
+
+                            completions.Add(response);
                         }
                     }
                 }
-
-                //var model = await document.GetSemanticModelAsync();
-                //var symbols = await Recommender.GetRecommendedSymbolsAtPositionAsync(model, position, _workspace);
-
-                //foreach (var symbol in symbols.Where(s => s.Name.IsValidCompletionFor(wordToComplete)))
-                //{
-                //    if (request.WantSnippet)
-                //    {
-                //        foreach (var completion in MakeSnippetedResponses(request, symbol))
-                //        {
-                //            completions.Add(completion);
-                //        }
-                //    }
-                //    else
-                //    {
-                //        completions.Add(MakeAutoCompleteResponse(request, symbol));
-                //    }
-                //}
             }
 
             return completions
