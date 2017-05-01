1ecfb2a0fb08c5280a37ef15bd157b9582e2dbc3
Dustin Campbell <dustin@teamcampbell.org>
2017-05-01 14:51:08 -0700

    Bring back old solution parsing API
    Solutions generated by Unity often have multiple projects with the same
    name but, the MSBuild solution parsing API throws an error if there
    are any projects with a duplicated name (this error can be seen when
    passing a Unity solution to MSBuild at the command-line). However,
    Visual Studio is resilient to solutions where projects have the same
    name, so we should be too. This change restores the solution parsing
    logic copied from https://github.com/dotnet/roslyn.

---
+++ src/OmniSharp.MSBuild/SolutionParsing/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace OmniSharp.MSBuild.SolutionParsing
+{
+    internal class Constants
+    {
+        public const string Invalid_project_block_expected_after_Project = "Invalid project block, expected \"=\" after Project.";
+        public const string Invalid_project_block_expected_after_project_name = "Invalid project block, expected \",\" after project name.";
+        public const string Invalid_project_block_expected_after_project_path = "Invalid project block, expected \",\" after project path.";
+        public const string Expected_0 = "Expected {0}.";
+        public const string _0_must_be_a_non_null_and_non_empty_string = "\"{0}\" must be a non-null and non-empty string.";
+        public const string Expected_header_colon_0 = "Expected header: \"{0}\".";
+        public const string Expected_end_of_file = "Expected end-of-file.";
+        public const string Expected_0_line = "Expected {0} line.";
+    }
+}
