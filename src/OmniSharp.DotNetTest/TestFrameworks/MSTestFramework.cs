9683497946f96f43d57a76af7781a2a31cbaaf9d
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2017-05-13 02:35:59 +0530

    Adding support for MSTest based tests in vscode.

---
+++ src/OmniSharp.DotNetTest/TestFrameworks/MSTestFramework.cs
@@ -0,0 +1,15 @@
+﻿namespace OmniSharp.DotNetTest.TestFrameworks
+{
+    internal class MSTestFramework : TestFramework
+    {
+        public override string FeatureName { get; } = "MSTestMethod";
+        public override string Name { get; } = "mstest";
+        public override string MethodArgument { get; } = "--test";
+
+        protected override bool IsTestAttributeName(string typeName)
+        {
+            return typeName == "Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute"
+                || typeName == "Microsoft.VisualStudio.TestTools.UnitTesting.DataTestMethodAttribute";
+        }
+    }
+}
