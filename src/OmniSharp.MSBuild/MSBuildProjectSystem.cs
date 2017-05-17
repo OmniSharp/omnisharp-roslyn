OmniSharp/omnisharp-roslyn

commit 2fbcc854f32ba959f71ca0019670ab3fc589a38f

Use .NET CLI to discover MSBuildSDKsPath

src/OmniSharp.MSBuild/MSBuildProjectSystem.cs
@@ -28,7 +28,7 @@ namespace OmniSharp.MSBuild
     {
         private readonly IOmniSharpEnvironment _environment;
         private readonly OmniSharpWorkspace _workspace;
-        private readonly DotNetCliService _dotNetCliService;
+        private readonly DotNetCliService _dotNetCli;
         private readonly MetadataFileReferenceCache _metadataFileReferenceCache;
         private readonly IEventEmitter _eventEmitter;
         private readonly IFileSystemWatcher _fileSystemWatcher;
@@ -58,7 +58,7 @@ namespace OmniSharp.MSBuild
         {
             _environment = environment;
             _workspace = workspace;
-            _dotNetCliService = dotNetCliService;
+            _dotNetCli = dotNetCliService;
             _metadataFileReferenceCache = metadataFileReferenceCache;
             _eventEmitter = eventEmitter;
             _fileSystemWatcher = fileSystemWatcher;
@@ -304,6 +304,22 @@ namespace OmniSharp.MSBuild
             return result.FilePath;
         }
 
+        private string GetSdksPath(string projectFilePath)
+        {
+            var info = _dotNetCli.GetInfo(Path.GetDirectoryName(projectFilePath));
+
+            if (info.IsEmpty && !string.IsNullOrWhiteSpace(info.BasePath))
+            {
+                return null;
+            }
+
+            var result = Path.Combine(info.BasePath, "Sdks");
+
+            return Directory.Exists(result)
+                ? result
+                : null;
+        }
+
         private ProjectFileInfo LoadProject(string projectFilePath)
         {
             _logger.LogInformation($"Loading project: {projectFilePath}");
@@ -313,7 +329,7 @@ namespace OmniSharp.MSBuild
 
             try
             {
-                project = ProjectFileInfo.Create(projectFilePath, _environment.TargetDirectory, _loggerFactory.CreateLogger<ProjectFileInfo>(), _options, diagnostics);
+                project = ProjectFileInfo.Create(projectFilePath, _environment.TargetDirectory, GetSdksPath(projectFilePath), _loggerFactory.CreateLogger<ProjectFileInfo>(), _options, diagnostics);
 
                 if (project == null)
                 {
@@ -339,7 +355,7 @@ namespace OmniSharp.MSBuild
                 if (_projects.TryGetValue(projectFilePath, out var oldProjectFileInfo))
                 {
                     var diagnostics = new List<MSBuildDiagnosticsMessage>();
-                    var newProjectFileInfo = oldProjectFileInfo.Reload(_environment.TargetDirectory, _loggerFactory.CreateLogger<ProjectFileInfo>(), _options, diagnostics);
+                    var newProjectFileInfo = oldProjectFileInfo.Reload(_environment.TargetDirectory, GetSdksPath(projectFilePath), _loggerFactory.CreateLogger<ProjectFileInfo>(), _options, diagnostics);
 
                     if (newProjectFileInfo != null)
                     {
@@ -574,7 +590,7 @@ namespace OmniSharp.MSBuild
             {
                 if (allowAutoRestore && _options.EnablePackageAutoRestore)
                 {
-                    _dotNetCliService.RestoreAsync(projectFileInfo.Directory, onFailure: () =>
+                    _dotNetCli.RestoreAsync(projectFileInfo.Directory, onFailure: () =>
                     {
                         FireUnresolvedDependenciesEvent(projectFileInfo, unresolvedDependencies);
                     });

src/OmniSharp.Abstractions/Services/DotNetCliService.cs
@@ -39,7 +39,6 @@ namespace OmniSharp.Services
             // the .NET CLI is not launched with the wrong values.
             environment.Remove("MSBUILD_EXE_PATH");
             environment.Remove("MSBuildExtensionsPath");
-            environment.Remove("MSBuildSDKsPath");
         }
 
         public void SetDotNetPath(string path)

src/OmniSharp.MSBuild/MSBuildEnvironment.cs
@@ -8,7 +8,6 @@ namespace OmniSharp.MSBuild
     {
         public const string MSBuildExePathName = "MSBUILD_EXE_PATH";
         public const string MSBuildExtensionsPathName = "MSBuildExtensionsPath";
-        public const string MSBuildSDKsPathName = "MSBuildSDKsPath";
 
         private static bool s_isInitialized;
 
@@ -16,7 +15,6 @@ namespace OmniSharp.MSBuild
 
         private static string s_msbuildExePath;
         private static string s_msbuildExtensionsPath;
-        private static string s_msbuildSDKsPath;
 
         public static bool IsInitialized => s_isInitialized;
 
@@ -47,15 +45,6 @@ namespace OmniSharp.MSBuild
             }
         }
 
-        public static string MSBuildSDKsPath
-        {
-            get
-            {
-                ThrowIfNotInitialized();
-                return s_msbuildSDKsPath;
-            }
-        }
-
         private static void ThrowIfNotInitialized()
         {
             if (!s_isInitialized)
@@ -79,12 +68,11 @@ namespace OmniSharp.MSBuild
                 s_usingVisualStudio = true;
                 s_isInitialized = true;
             }
-            else if (TryWithLocalMSBuild(logger, out var msbuildExePath, out var msbuildExtensionsPath, out var msbuildSDKsPath))
+            else if (TryWithLocalMSBuild(logger, out var msbuildExePath, out var msbuildExtensionsPath))
             {
                 logger.LogInformation("MSBuild will use local OmniSharp installation.");
                 s_msbuildExePath = msbuildExePath;
                 s_msbuildExtensionsPath = msbuildExtensionsPath;
-                s_msbuildSDKsPath = msbuildSDKsPath;
                 s_isInitialized = true;
             }
 
@@ -94,11 +82,10 @@ namespace OmniSharp.MSBuild
             }
         }
 
-        private static bool TryWithLocalMSBuild(ILogger logger, out string msbuildExePath, out string msbuildExtensionsPath, out string msbuildSDKsPath)
+        private static bool TryWithLocalMSBuild(ILogger logger, out string msbuildExePath, out string msbuildExtensionsPath)
         {
             msbuildExePath = null;
             msbuildExtensionsPath = null;
-            msbuildSDKsPath = null;
 
             var msbuildDirectory = FindMSBuildDirectory(logger);
             if (msbuildDirectory == null)
@@ -118,22 +105,11 @@ namespace OmniSharp.MSBuild
             // Set the MSBuildExtensionsPath environment variable to the local msbuild directory.
             msbuildExtensionsPath = msbuildDirectory;
 
-            // Set the MSBuildSDKsPath environment variable to the location of the SDKs.
-            msbuildSDKsPath = FindMSBuildSDKsPath(msbuildDirectory);
-            if (msbuildSDKsPath == null)
-            {
-                logger.LogError("Could not locate MSBuild Sdks path");
-                return false;
-            }
-
             Environment.SetEnvironmentVariable(MSBuildExePathName, msbuildExePath);
             logger.LogInformation($"{MSBuildExePathName} environment variable set to {msbuildExePath}");
 
             Environment.SetEnvironmentVariable(MSBuildExtensionsPathName, msbuildExtensionsPath);
             logger.LogInformation($"{MSBuildExtensionsPathName} environment variable set to {msbuildExtensionsPath}");
-
-            Environment.SetEnvironmentVariable(MSBuildSDKsPathName, msbuildSDKsPath);
-            logger.LogInformation($"{MSBuildSDKsPathName} environment variable set to {msbuildSDKsPath}");
 
             return true;
         }
@@ -200,15 +176,6 @@ namespace OmniSharp.MSBuild
             }
 
             return null;
-        }
-
-        private static string FindMSBuildSDKsPath(string directory)
-        {
-            var msbuildSDKsPath = Path.Combine(directory, "Sdks");
-
-            return Directory.Exists(msbuildSDKsPath)
-                ? msbuildSDKsPath
-                : null;
         }
     }
 }

src/OmniSharp.MSBuild/ProjectFile/ProjectFileInfo.cs
@@ -70,7 +70,7 @@ namespace OmniSharp.MSBuild.ProjectFile
         }
 
         public static ProjectFileInfo Create(
-            string filePath, string solutionDirectory, ILogger logger,
+            string filePath, string solutionDirectory, string sdksPath, ILogger logger,
             MSBuildOptions options = null, ICollection<MSBuildDiagnosticsMessage> diagnostics = null)
         {
             if (!File.Exists(filePath))
@@ -78,7 +78,7 @@ namespace OmniSharp.MSBuild.ProjectFile
                 return null;
             }
 
-            var projectInstance = LoadProject(filePath, solutionDirectory, logger, options, diagnostics, out var targetFrameworks);
+            var projectInstance = LoadProject(filePath, solutionDirectory, sdksPath, logger, options, diagnostics, out var targetFrameworks);
             if (projectInstance == null)
             {
                 return null;
@@ -91,12 +91,12 @@ namespace OmniSharp.MSBuild.ProjectFile
         }
 
         private static ProjectInstance LoadProject(
-            string filePath, string solutionDirectory, ILogger logger,
+            string filePath, string solutionDirectory, string sdksPath, ILogger logger,
             MSBuildOptions options, ICollection<MSBuildDiagnosticsMessage> diagnostics, out ImmutableArray<string> targetFrameworks)
         {
             options = options ?? new MSBuildOptions();
 
-            var globalProperties = GetGlobalProperties(options, solutionDirectory, logger);
+            var globalProperties = GetGlobalProperties(options, solutionDirectory, sdksPath, logger);
 
             var collection = new ProjectCollection(globalProperties);
 
@@ -168,10 +168,10 @@ namespace OmniSharp.MSBuild.ProjectFile
         }
 
         public ProjectFileInfo Reload(
-            string solutionDirectory, ILogger logger,
+            string solutionDirectory, string sdksPath, ILogger logger,
             MSBuildOptions options = null, ICollection<MSBuildDiagnosticsMessage> diagnostics = null)
         {
-            var projectInstance = LoadProject(FilePath, solutionDirectory, logger, options, diagnostics, out var targetFrameworks);
+            var projectInstance = LoadProject(FilePath, solutionDirectory, sdksPath, logger, options, diagnostics, out var targetFrameworks);
             if (projectInstance == null)
             {
                 return null;
@@ -193,7 +193,7 @@ namespace OmniSharp.MSBuild.ProjectFile
             });
         }
 
-        private static Dictionary<string, string> GetGlobalProperties(MSBuildOptions options, string solutionDirectory, ILogger logger)
+        private static Dictionary<string, string> GetGlobalProperties(MSBuildOptions options, string solutionDirectory, string sdksPath, ILogger logger)
         {
             var globalProperties = new Dictionary<string, string>
             {
@@ -211,7 +211,7 @@ namespace OmniSharp.MSBuild.ProjectFile
             globalProperties.AddPropertyIfNeeded(
                 PropertyNames.MSBuildSDKsPath,
                 userOptionValue: options.MSBuildSDKsPath,
-                environmentValue: MSBuildEnvironment.MSBuildSDKsPath);
+                environmentValue: sdksPath);
 
             globalProperties.AddPropertyIfNeeded(
                 PropertyNames.VisualStudioVersion,

tests/OmniSharp.MSBuild.Tests/ProjectFileInfoTests.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using OmniSharp.MSBuild.ProjectFile;
+using OmniSharp.Services;
 using TestUtility;
 using Xunit;
 using Xunit.Abstractions;
@@ -25,14 +26,23 @@ namespace OmniSharp.MSBuild.Tests
             }
         }
 
+        private static string GetSdksPath(OmniSharpTestHost host)
+        {
+            var dotNetCli = host.GetExport<DotNetCliService>();
+            var info = dotNetCli.GetInfo();
+
+            return Path.Combine(info.BasePath, "Sdks");
+        }
+
         [Fact]
         public async Task HelloWorld_has_correct_property_values()
         {
-            using (var testProejct = await _testAssets.GetTestProjectAsync("HelloWorld"))
+            using (var host = CreateOmniSharpHost())
+            using (var testProject = await _testAssets.GetTestProjectAsync("HelloWorld"))
             {
-                var projectFilePath = Path.Combine(testProejct.Directory, "HelloWorld.csproj");
+                var projectFilePath = Path.Combine(testProject.Directory, "HelloWorld.csproj");
 
-                var projectFileInfo = ProjectFileInfo.Create(projectFilePath, testProejct.Directory, this._logger);
+                var projectFileInfo = ProjectFileInfo.Create(projectFilePath, testProject.Directory, GetSdksPath(host), this._logger);
 
                 Assert.NotNull(projectFileInfo);
                 Assert.Equal(projectFilePath, projectFileInfo.FilePath);
@@ -46,11 +56,12 @@ namespace OmniSharp.MSBuild.Tests
         [Fact]
         public async Task HelloWorldSlim_has_correct_property_values()
         {
+            using (var host = CreateOmniSharpHost())
             using (var testProject = await _testAssets.GetTestProjectAsync("HelloWorldSlim"))
             {
                 var projectFilePath = Path.Combine(testProject.Directory, "HelloWorldSlim.csproj");
 
-                var projectFileInfo = ProjectFileInfo.Create(projectFilePath, testProject.Directory, this._logger);
+                var projectFileInfo = ProjectFileInfo.Create(projectFilePath, testProject.Directory, GetSdksPath(host), this._logger);
 
                 Assert.NotNull(projectFileInfo);
                 Assert.Equal(projectFilePath, projectFileInfo.FilePath);
@@ -64,11 +75,12 @@ namespace OmniSharp.MSBuild.Tests
         [Fact]
         public async Task NetStandardAndNetCoreApp_has_correct_property_values()
         {
+            using (var host = CreateOmniSharpHost())
             using (var testProject = await _testAssets.GetTestProjectAsync("NetStandardAndNetCoreApp"))
             {
                 var projectFilePath = Path.Combine(testProject.Directory, "NetStandardAndNetCoreApp.csproj");
 
-                var projectFileInfo = ProjectFileInfo.Create(projectFilePath, testProject.Directory, this._logger);
+                var projectFileInfo = ProjectFileInfo.Create(projectFilePath, testProject.Directory, GetSdksPath(host), this._logger);
 
                 Assert.NotNull(projectFileInfo);
                 Assert.Equal(projectFilePath, projectFileInfo.FilePath);

commit 0e500d5582329214d44a8ed1f4b1294fed527140

Fix Path.Combine exception in MSBuildProjectSystem.GetSdksPath
This fixes regression introduced in 2fbcc854f, where null info.BasePath
is passed to Path.Combine causing exception.

src/OmniSharp.MSBuild/MSBuildProjectSystem.cs
@@ -308,7 +308,7 @@ namespace OmniSharp.MSBuild
         {
             var info = _dotNetCli.GetInfo(Path.GetDirectoryName(projectFilePath));
 
-            if (info.IsEmpty && !string.IsNullOrWhiteSpace(info.BasePath))
+            if (info.IsEmpty || string.IsNullOrWhiteSpace(info.BasePath))
             {
                 return null;
             }