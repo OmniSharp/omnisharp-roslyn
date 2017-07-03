a651d6ade8d00bc329b11e8f9048280cb7a9da9e
Martin Björkström <martin.bjorkstrom@gmail.com>
2017-07-03 22:46:11 +0300

    Adds OmniSharp.Cake project and Cake project system

---
+++ src/OmniSharp.Cake/CakeProjectSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using OmniSharp.Models.WorkspaceInformation;
+using OmniSharp.Services;
+
+namespace OmniSharp.Cake
+{
+    [Export(typeof(IProjectSystem)), Shared]
+    public class CakeProjectSystem : IProjectSystem
+    {
+        public string Key => "Cake";
+        public string Language => Constants.LanguageNames.Cake;
+        public IEnumerable<string> Extensions => new[] { ".cake" };
+
+        public void Initalize(IConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetWorkspaceModelAsync(WorkspaceInformationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetProjectModelAsync(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/OmniSharp.Cake/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace OmniSharp.Cake
+{
+    internal class Constants
+    {
+        public static class LanguageNames
+        {
+            public const string Cake = "Cake";
+        }
+    }
+}
