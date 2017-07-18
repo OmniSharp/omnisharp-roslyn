685ff696f21de2b8a27581e8a43f675bd727e22a
Dustin Campbell <dustin@teamcampbell.org>
2017-07-18 15:05:33 -0700

    Flesh out solution parsing API and add tests

---
+++ src/OmniSharp.MSBuild/SolutionParsing/GlobalSectionBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+
+namespace OmniSharp.MSBuild.SolutionParsing
+{
+    internal sealed class GlobalSectionBlock : SectionBlock
+    {
+        private GlobalSectionBlock(string name, ImmutableArray<Property> properties)
+            : base(name, properties)
+        {
+        }
+
+        public static GlobalSectionBlock Parse(string headerLine, Scanner scanner)
+        {
+            var (name, properties) = ParseNameAndProperties(
+                "GlobalSection", "EndGlobalSection", headerLine, scanner);
+
+            return new GlobalSectionBlock(name, properties);
+        }
+    }
+}
