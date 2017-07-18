bd66e71539641db775f7d3df8f8219034eee2b3d
Dustin Campbell <dustin@teamcampbell.org>
2017-07-18 11:33:40 -0700

    Add SolutionFile class and initial test

---
+++ src/OmniSharp.MSBuild/SolutionParsing/SolutionFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OmniSharp.MSBuild.SolutionParsing
+{
+    internal sealed class SolutionFile
+    {
+        private SolutionFile()
+        {
+        }
+
+        public static SolutionFile Parse(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
+            return new SolutionFile();
+        }
+    }
+}
---
+++ tests/OmniSharp.MSBuild.Tests/SolutionParsingTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OmniSharp.MSBuild.SolutionParsing;
+using Xunit;
+
+namespace OmniSharp.MSBuild.Tests
+{
+    public class SolutionParsingTests
+    {
+        [Fact]
+        public void SolutionFile_Parse_throws_with_null_text()
+        {
+            Assert.Throws<ArgumentNullException>(() => SolutionFile.Parse(null));
+        }
+    }
+}
