72a584df8680a1f0b95aa401154dff4173fd1fee
filipw <wojcieszyn@gmail.com>
2017-08-16 10:38:43 +1000

    do not crash when unloadable assembly is discovered as part of host services

--- src/OmniSharp.Roslyn/HostServicesAggregator.cs
+++ src/OmniSharp.Roslyn/HostServicesAggregator.cs
@@ -5,18 +5,22 @@ using System.Reflection;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Host.Mef;
 using OmniSharp.Services;
+using System.Linq;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace OmniSharp
 {
     [Export]
     public class HostServicesAggregator
     {
-        private readonly ImmutableArray<Assembly> _assemblies;
+        private ImmutableArray<Assembly> _assemblies;
 
         [ImportingConstructor]
         public HostServicesAggregator(
-            [ImportMany] IEnumerable<IHostServicesProvider> hostServicesProviders)
+            [ImportMany] IEnumerable<IHostServicesProvider> hostServicesProviders, ILoggerFactory loggerFactory)
         {
+            var logger = loggerFactory.CreateLogger<HostServicesAggregator>();
             var builder = ImmutableHashSet.CreateBuilder<Assembly>();
 
             // We always include the default Roslyn assemblies, which includes:
@@ -34,7 +38,18 @@ namespace OmniSharp
             {
                 foreach (var assembly in provider.Assemblies)
                 {
-                    builder.Add(assembly);
+                    try
+                    {
+                        var exportedTypes = assembly.ExportedTypes;
+                        builder.Add(assembly);
+                        logger.LogTrace("Successfully added {assembly} to host service assemblies.", assembly.FullName);
+                    }
+                    catch (Exception)
+                    {
+                        // if we can't see exported types, it means that the assembly cannot participate
+                        // in MefHostServices as one or more of its dependencies (typically a Visual Studio or GACed DLL) is missing
+                        logger.LogWarning("Expected to use {assembly} in host services but the assembly cannot be used due to missing dependencies.", assembly.FullName);
+                    }
                 }
             }
 
