47804cf4de61960013f45259ed1db513fbf409cd
Ravi Chande <rchande@microsoft.com>
2017-10-24 10:13:12 -0700

    Update IFileSystemWatcher to: * Pass through FileChangeTypes when available * Allow clients to watch a directory (in addition to individual files)

--- src/OmniSharp.Abstractions/FileWatching/IFileSystemWatcher.cs
+++ src/OmniSharp.Abstractions/FileWatching/IFileSystemWatcher.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using OmniSharp.Models;
+using System;
 
 namespace OmniSharp.FileWatching
 {
     // TODO: Flesh out this API more
     public interface IFileSystemWatcher
     {
-        void Watch(string path, Action<string> callback);
+        void Watch(string path, Action<string, FileChangeType?> callback);
 
-        void TriggerChange(string path);
+        /// <summary>
+        /// Called when a file is created, changed, or deleted.
+        /// </summary>
+        /// <param name="path">The path to the file</param>
+        /// <param name="verb">The type of change. Hosts are not required to pass a change type</param>
+        void TriggerChange(string path, FileChangeType? verb);
+
+        void WatchDirectory(string path, Action<string, FileChangeType?> callback);
     }
 }
