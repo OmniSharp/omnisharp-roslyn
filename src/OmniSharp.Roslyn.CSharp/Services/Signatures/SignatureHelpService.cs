6362fdab1dc5e5fa1ba8b415355a27468dbc69f1
Akshita Agarwal <akshita.agarwal.cse13@itbhu.ac.in>
2017-11-27 11:01:39 -0800

    Unnecessary using removed

--- src/OmniSharp.Roslyn.CSharp/Services/Signatures/SignatureHelpService.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/Signatures/SignatureHelpService.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Composition;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
