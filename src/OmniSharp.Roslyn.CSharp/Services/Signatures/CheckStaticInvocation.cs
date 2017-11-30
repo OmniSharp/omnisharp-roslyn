a13a6b0cffea2f58805dc6d30692116f5279f639
Akshita Agarwal <akshita.agarwal.cse13@itbhu.ac.in>
2017-11-29 16:19:40 -0800

    Added Attribution

--- src/OmniSharp.Roslyn.CSharp/Services/Signatures/CheckStaticInvocation.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/Signatures/CheckStaticInvocation.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-
+// Adapted from https://github.com/dotnet/roslyn/blob/master/src/Workspaces/CSharp/Portable/Extensions/SyntaxNodeExtensions.cs
 using System;
 using System.Collections.Generic;
 using System.Linq;
