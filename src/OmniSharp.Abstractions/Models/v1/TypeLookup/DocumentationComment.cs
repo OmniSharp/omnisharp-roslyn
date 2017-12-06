90a236ff83a3df4b8995bce1ee624a948eeff03b
Akshita Agarwal <akshita.agarwal.cse13@itbhu.ac.in>
2017-12-05 16:43:27 -0800

    Added the code for forming the comment object into the documentation comment class itself

--- src/OmniSharp.Abstractions/Models/v1/TypeLookup/DocumentationComment.cs
+++ src/OmniSharp.Abstractions/Models/v1/TypeLookup/DocumentationComment.cs
@@ -1,23 +1,171 @@
-﻿namespace OmniSharp.Models.TypeLookup
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace OmniSharp.Models.TypeLookup
 {
     public class DocumentationComment
     {
-        public string RemarksText { get; set; }
-        public string ExampleText { get; set; }
-        public string ReturnsText { get; set; }
-        public string SummaryText { get; set; }
-        public string ValueText { get; set; }
-        public string[ ] ParamElements { get; set; }
-        public string[ ] TypeParamElements { get; set; }
-        public string[ ] Exception { get; set; }
+        public string RemarksText { get; }
+        public string ExampleText { get; }
+        public string ReturnsText { get; }
+        public string SummaryText { get; }
+        public string ValueText { get; }
+        public string[ ] ParamElements { get; }
+        public string[ ] TypeParamElements { get; }
+        public string[ ] Exception { get; }
 
-        public DocumentationComment()
+        private DocumentationComment(string remarksText, string exampleText, string returnsText, string summaryText, string valueText, string [ ] paramElements, string [ ] typeParamElements, string [ ] exception)
         {
-            RemarksText = "";
-            ExampleText = "";
-            ReturnsText = "";
-            SummaryText = "";
-            ValueText = "";
+            RemarksText = remarksText;
+            ExampleText = exampleText;
+            ReturnsText = returnsText;
+            SummaryText = summaryText;
+            ValueText = valueText;
+            ParamElements = paramElements;
+            TypeParamElements = typeParamElements;
+            Exception = exception;
+        }
+
+        public static DocumentationComment From(string xmlDocumentation, string lineEnding)
+        {
+            var reader = new StringReader("<docroot>" + xmlDocumentation + "</docroot>");
+            StringBuilder remarksText = new StringBuilder();
+            StringBuilder exampleText = new StringBuilder();
+            StringBuilder returnsText = new StringBuilder();
+            StringBuilder summaryText = new StringBuilder();
+            StringBuilder valueText = new StringBuilder();
+            List<StringBuilder> paramElements = new List<StringBuilder>();
+            List<StringBuilder> typeParamElements = new List<StringBuilder>();
+            List<StringBuilder> exception = new List<StringBuilder>();
+
+            using (var xml = XmlReader.Create(reader))
+            {
+                try
+                {
+                    xml.Read();
+                    string elementName = null;
+                    StringBuilder currentSectionBuilder = null;
+                    do
+                    {
+                        if (xml.NodeType == XmlNodeType.Element)
+                        {
+                            elementName = xml.Name.ToLowerInvariant();
+                            switch (elementName)
+                            {
+                                case "filterpriority":
+                                    xml.Skip();
+                                    break;
+                                case "remarks":
+                                    remarksText.Append("Remarks: ");
+                                    currentSectionBuilder = remarksText;
+                                    break;
+                                case "example":
+                                    exampleText.Append("Example: ");
+                                    currentSectionBuilder = exampleText;
+                                    break;
+                                case "exception":
+                                    StringBuilder ExceptionInstance = new StringBuilder();
+                                    ExceptionInstance.Append(GetCref(xml["cref"]).TrimEnd());
+                                    ExceptionInstance.Append(": ");
+                                    currentSectionBuilder = ExceptionInstance;
+                                    exception.Add(ExceptionInstance);
+                                    break;
+                                case "returns":
+                                    returnsText.Append("Returns: ");
+                                    currentSectionBuilder = returnsText;
+                                    break;
+                                case "summary":
+                                    summaryText.Append("Summary: ");
+                                    currentSectionBuilder = summaryText;
+                                    break;
+                                case "see":
+                                    currentSectionBuilder.Append(GetCref(xml["cref"]));
+                                    currentSectionBuilder.Append(xml["langword"]);
+                                    break;
+                                case "seealso":
+                                    currentSectionBuilder.Append("See also: ");
+                                    currentSectionBuilder.Append(GetCref(xml["cref"]));
+                                    break;
+                                case "paramref":
+                                    currentSectionBuilder.Append(xml["name"]);
+                                    currentSectionBuilder.Append(" ");
+                                    break;
+                                case "param":
+                                    StringBuilder paramInstance = new StringBuilder();
+                                    paramInstance.Append(TrimMultiLineString(xml["name"], lineEnding));
+                                    paramInstance.Append(": ");
+                                    currentSectionBuilder = paramInstance;
+                                    paramElements.Add(paramInstance);
+                                    break;
+                                case "typeparamref":
+                                    currentSectionBuilder.Append(xml["name"]);
+                                    currentSectionBuilder.Append(" ");
+                                    break;
+                                case "typeparam":
+                                    StringBuilder typeParamInstance = new StringBuilder();
+                                    typeParamInstance.Append(TrimMultiLineString(xml["name"], lineEnding));
+                                    typeParamInstance.Append(": ");
+                                    currentSectionBuilder = typeParamInstance;
+                                    typeParamElements.Add(typeParamInstance);
+                                    break;
+                                case "value":
+                                    valueText.Append("Value: ");
+                                    currentSectionBuilder = valueText;
+                                    break;
+                                case "br":
+                                case "para":
+                                    currentSectionBuilder.Append(lineEnding);
+                                    break;
+                            }
+                        }
+                        else if (xml.NodeType == XmlNodeType.Text && currentSectionBuilder != null)
+                        {
+                            if (elementName == "code")
+                            {
+                                currentSectionBuilder.Append(xml.Value);
+                            }
+                            else
+                            {
+                                currentSectionBuilder.Append(TrimMultiLineString(xml.Value, lineEnding));
+                            }
+                        }
+                    } while (xml.Read());
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+            return new DocumentationComment(remarksText.ToString(),exampleText.ToString(),returnsText.ToString(),summaryText.ToString(),valueText.ToString(),
+                paramElements.Select(s => s.ToString()).ToArray(),typeParamElements.Select(s => s.ToString()).ToArray(),
+                exception.Select(s => s.ToString()).ToArray());
+        }
+
+        private static string TrimMultiLineString(string input, string lineEnding)
+        {
+            var lines = input.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join(lineEnding, lines.Select(l => l.TrimStart()));
+        }
+
+        private static string GetCref(string cref)
+        {
+            if (cref == null || cref.Trim().Length == 0)
+            {
+                return "";
+            }
+            if (cref.Length < 2)
+            {
+                return cref;
+            }
+            if (cref.Substring(1, 1) == ":")
+            {
+                return cref.Substring(2, cref.Length - 2) + " ";
+            }
+            return cref + " ";
         }
     }
 }
