a6876e1c041db158eb71f6e6323f81a5813ad92f
Akshita Agarwal <akshita.agarwal.cse13@itbhu.ac.in>
2018-01-08 15:57:50 -0800

    Removed method name from run test in class

--- src/OmniSharp.DotNetTest/Models/RunTestInClassRequest.cs
+++ src/OmniSharp.DotNetTest/Models/RunTestInClassRequest.cs
@@ -6,7 +6,6 @@ namespace OmniSharp.DotNetTest.Models
     [OmniSharpEndpoint(OmniSharpEndpoints.V2.RunAllTestsInClass, typeof(RunTestsInClassRequest), typeof(RunTestResponse[]))]
     public class RunTestsInClassRequest : Request
     {
-        public string MethodName { get; set; }
         public string TestFrameworkName { get; set; }
         /// <summary>
         /// e.g. .NETCoreApp, Version=2.0
