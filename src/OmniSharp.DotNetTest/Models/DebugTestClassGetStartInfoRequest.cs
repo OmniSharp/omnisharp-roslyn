61672984bcdb4b05e05d28cc8af33dfa9d4b6914
Akshita Agarwal <akshita.agarwal.cse13@itbhu.ac.in>
2018-01-09 14:35:43 -0800

    Added code for debug all tests

---
+++ src/OmniSharp.DotNetTest/Models/DebugTestClassGetStartInfoRequest.cs
@@ -0,0 +1,16 @@
+﻿using OmniSharp.Mef;
+using OmniSharp.Models;
+
+namespace OmniSharp.DotNetTest.Models
+{
+    [OmniSharpEndpoint(OmniSharpEndpoints.V2.DebugTestsInClassGetStartInfo, typeof(DebugTestClassGetStartInfoRequest), typeof(DebugTestGetStartInfoResponse[]))]
+    class DebugTestClassGetStartInfoRequest :  Request
+    {
+        public string[] MethodsInClass { get; set; }
+        public string TestFrameworkName { get; set; }
+        /// <summary>
+        /// e.g. .NETCoreApp, Version=2.0
+        /// </summary>
+        public string TargetFrameworkVersion { get; set; }
+    }
+}
