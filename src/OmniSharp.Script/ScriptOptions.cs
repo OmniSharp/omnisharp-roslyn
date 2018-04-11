8de57c2b0e917208853e0e945d6564b2abcad67e
filipw <wojcieszyn@gmail.com>
2018-04-11 15:42:28 +0200

    added typed script options

---
+++ src/OmniSharp.Script/ScriptOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace OmniSharp.Script
+{
+    public class ScriptOptions
+    {
+        public bool EnableScriptNuGetReferences { get; set; }
+
+        public string DefaultTargetFramework { get; set; } = "net46";
+    }
+}
