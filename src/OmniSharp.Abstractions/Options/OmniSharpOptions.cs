d1c72b20fd8641e8fa783af597e139e13267fda1
filipw <wojcieszyn@gmail.com>
2018-04-12 20:57:57 +0200

    added new file options to OmniSharpOptions

--- src/OmniSharp.Abstractions/Options/OmniSharpOptions.cs
+++ src/OmniSharp.Abstractions/Options/OmniSharpOptions.cs
@@ -7,5 +7,13 @@ namespace OmniSharp.Options
         public RoslynExtensionsOptions RoslynExtensionsOptions { get; } = new RoslynExtensionsOptions();
 
         public FormattingOptions FormattingOptions { get; } = new FormattingOptions();
+
+        public FileOptions FileOptions { get; } = new FileOptions();
+    }
+
+    public class FileOptions
+    {
+        public string[] ExcludeSearchPatterns { get; set; } = new[] { "**/node_modules/**/*", "**/bin/**/*", "**/obj/**/*" };
+
     }
 }
