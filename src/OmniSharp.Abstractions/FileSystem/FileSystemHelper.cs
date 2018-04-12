8a0478e1b08ef266f1eed664bba039353344e427
filipw <wojcieszyn@gmail.com>
2018-04-12 20:59:01 +0200

    added a new file system helper with support for globbing

---
+++ src/OmniSharp.Abstractions/FileSystem/FileSystemHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using OmniSharp.Options;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+namespace OmniSharp.FileSystem
+{
+    [Export, Shared]
+    public class FileSystemHelper
+    {
+        private readonly OmniSharpOptions _omniSharpOptions;
+        private readonly IOmniSharpEnvironment _omniSharpEnvironment;
+
+        [ImportingConstructor]
+        public FileSystemHelper(OmniSharpOptions omniSharpOptions, IOmniSharpEnvironment omniSharpEnvironment)
+        {
+            _omniSharpOptions = omniSharpOptions;
+            _omniSharpEnvironment = omniSharpEnvironment;
+        }
+
+        public IEnumerable<string> GetFiles(string includePattern) => GetFiles(includePattern, _omniSharpEnvironment.TargetDirectory);
+
+        public IEnumerable<string> GetFiles(string includePattern, string targetDirectory)
+        {
+            var matcher = new Matcher();
+            matcher.AddInclude(includePattern);
+
+            if (_omniSharpOptions.FileOptions.ExcludeSearchPatterns != null && _omniSharpOptions.FileOptions.ExcludeSearchPatterns.Any())
+            {
+                matcher.AddExcludePatterns(_omniSharpOptions.FileOptions.ExcludeSearchPatterns);
+            }
+
+            return matcher.GetResultsInFullPath(targetDirectory);
+        }
+    }
+}
