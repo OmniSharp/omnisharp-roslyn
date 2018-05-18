042798e721896d9287e331ea9b2103be507586f3
filipw <wojcieszyn@gmail.com>
2018-05-18 20:56:00 +0200

    added a test for 2 csi scripts

--- tests/OmniSharp.Script.Tests/WorkspaceInformationTests.cs
+++ tests/OmniSharp.Script.Tests/WorkspaceInformationTests.cs
@@ -32,12 +32,38 @@ namespace OmniSharp.Script.Tests
                 Assert.Equal("main.csx", Path.GetFileName(project.Path));
 
                 // should have reference to mscorlib
-                Assert.Contains(project.ImplicitAssemblyReferences, a => a == Assembly.Load(new AssemblyName("mscorlib"))?.Location);
+                Assert.Contains(project.ImplicitAssemblyReferences, a => a == GetMsCorlibPath());
 
                 // default globals object
                 Assert.Equal(typeof(CommandLineScriptGlobals), project.GlobalsType);
             }
         }
+
+        [Fact]
+        public async Task TwoCsiScripts()
+        {
+            using (var testProject = TestAssets.Instance.GetTestScript("TwoCsiScripts"))
+            using (var host = CreateOmniSharpHost(testProject.Directory))
+            {
+                var workspaceInfo = await GetWorkspaceInfoAsync(host);
+
+                var scriptProjects = workspaceInfo.Projects.ToArray();
+                Assert.Equal(2, scriptProjects.Length);
+
+                Assert.Equal("main.csx", Path.GetFileName(scriptProjects[0].Path));
+                Assert.Equal("users.csx", Path.GetFileName(scriptProjects[1].Path));
+
+                // should have reference to mscorlib
+                Assert.Contains(scriptProjects[0].ImplicitAssemblyReferences, a => a == GetMsCorlibPath());
+                Assert.Contains(scriptProjects[1].ImplicitAssemblyReferences, a => a == GetMsCorlibPath());
+
+                // default globals object
+                Assert.Equal(typeof(CommandLineScriptGlobals), scriptProjects[0].GlobalsType);
+                Assert.Equal(typeof(CommandLineScriptGlobals), scriptProjects[1].GlobalsType);
+            }
+        }
+
+        private string GetMsCorlibPath() => Assembly.Load(new AssemblyName("mscorlib"))?.Location;
 
         private static async Task<ScriptContextModelCollection> GetWorkspaceInfoAsync(OmniSharpTestHost host)
         {
