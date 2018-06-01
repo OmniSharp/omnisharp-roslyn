c4782e79354be513e0729790229e4d975188e4f1
Dustin Campbell <dustin@teamcampbell.org>
2018-06-01 04:31:17 -0700

    Add initial CodeStructureService and unit test facts

---
+++ src/OmniSharp.Roslyn.CSharp/Services/Structure/CodeStructureService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using OmniSharp.Mef;
+using OmniSharp.Models.V2.CodeStructure;
+
+namespace OmniSharp.Roslyn.CSharp.Services.Structure
+{
+    [OmniSharpHandler(OmniSharpEndpoints.V2.CodeStructure, LanguageNames.CSharp)]
+    public class CodeStructureService : IRequestHandler<CodeStructureRequest, CodeStructureResponse>
+    {
+        private readonly OmniSharpWorkspace _workspace;
+
+        [ImportingConstructor]
+        public CodeStructureService(OmniSharpWorkspace workspace)
+        {
+            _workspace = workspace;
+        }
+
+        public Task<CodeStructureResponse> Handle(CodeStructureRequest request)
+        {
+            var document = _workspace.GetDocument(request.FileName);
+            if (document == null)
+            {
+                return null;
+            }
+
+            var response = new CodeStructureResponse
+            {
+                Elements = Array.Empty<CodeElement>()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ tests/OmniSharp.Roslyn.CSharp.Tests/CodeStructureFacts.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using OmniSharp.Models.V2.CodeStructure;
+using OmniSharp.Roslyn.CSharp.Services.Structure;
+using TestUtility;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OmniSharp.Roslyn.CSharp.Tests
+{
+    public class CodeStructureFacts : AbstractSingleRequestHandlerTestFixture<CodeStructureService>
+    {
+        public CodeStructureFacts(ITestOutputHelper output, SharedOmniSharpHostFixture sharedOmniSharpHostFixture)
+            : base(output, sharedOmniSharpHostFixture)
+        {
+        }
+
+        protected override string EndpointName => OmniSharpEndpoints.V2.CodeStructure;
+
+        [Fact]
+        public async Task AllTypes()
+        {
+            const string source = @"
+class C { }
+delegate void D();
+enum E { One, Two, Three }
+interface I { }
+struct S { }
+";
+
+            var response = await GetCodeStructureAsync(source);
+        }
+
+        private Task<CodeStructureResponse> GetCodeStructureAsync(string source)
+        {
+            var testFile = new TestFile("test.cs", source);
+            SharedOmniSharpTestHost.AddFilesToWorkspace(testFile);
+
+            var requestHandler = GetRequestHandler(SharedOmniSharpTestHost);
+
+            var request = new CodeStructureRequest
+            {
+                FileName = "test.cs"
+            };
+
+            return requestHandler.Handle(request);
+        }
+    }
+}
