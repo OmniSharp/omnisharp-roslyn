774cc9b793157bd324b09e1c1853114e680a55fb
Unknown <akagarw@microsoft.com>
2018-07-04 13:46:13 -0700

    Orphan file system using the msbuild system

--- src/OmniSharp.OrphanFiles/OrphanFileSystem.cs
+++ src/OmniSharp.OrphanFiles/OrphanFileSystem.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OmniSharp.FileSystem;
 using OmniSharp.FileWatching;
 using OmniSharp.Models.WorkspaceInformation;
@@ -18,7 +19,7 @@ namespace OmniSharp.OrphanFiles
     [Export(typeof(IProjectSystem)), Shared]
     public class OrphanFileSystem : IProjectSystem
     {
-        public string Key { get; } = "MsBuild";
+        public string Key { get; } = "OrphanFiles";
         public string Language { get; } = LanguageNames.CSharp;
         IEnumerable<string> IProjectSystem.Extensions => throw new NotImplementedException();
         public bool EnabledByDefault { get; } = true;
@@ -27,53 +28,61 @@ namespace OmniSharp.OrphanFiles
         private readonly OmniSharpWorkspace _workspace;
         private readonly IFileSystemWatcher _fileSystemWatcher;
         private readonly FileSystemHelper _fileSystemHelper;
-        private readonly ProjectSystem _msbuildSystem; 
+        private readonly IProjectSystem _projectSystem;
+        private readonly ILogger _logger;
 
         [ImportingConstructor]
-        public OrphanFileSystem(OmniSharpWorkspace workspace, IFileSystemWatcher fileSystemWatcher, FileSystemHelper fileSystemHelper, [Import] ProjectSystem msbuildSystem)
+        public OrphanFileSystem(OmniSharpWorkspace workspace, IFileSystemWatcher fileSystemWatcher, FileSystemHelper fileSystemHelper, ILoggerFactory loggerFactory, [Import] ProjectSystem projectSystem)
         {
             _workspace = workspace;
             _fileSystemWatcher = fileSystemWatcher ?? throw new ArgumentNullException(nameof(fileSystemWatcher));
             _fileSystemHelper = fileSystemHelper;
-            _msbuildSystem = msbuildSystem;
+            _logger = loggerFactory.CreateLogger<OrphanFileSystem>();
+            _projectSystem = projectSystem;
         }
 
         Task<object> IProjectSystem.GetProjectModelAsync(string filePath)
         {
-            throw new NotImplementedException();
+            return Task.FromResult<object>(null);
         }
 
         Task<object> IProjectSystem.GetWorkspaceModelAsync(WorkspaceInformationRequest request)
         {
-            throw new NotImplementedException();
+            return Task.FromResult<object>(null);
         }
 
         void IProjectSystem.Initalize(IConfiguration configuration)
         {
             var allFiles = _fileSystemHelper.GetFiles("**/*.cs");
             var solution = _workspace.CurrentSolution;
-            string assemblyName = Guid.NewGuid().ToString("N");
 
             foreach (var file in allFiles)
             {
-                var project = await _projectEventforwarder.GetProjectInformationAsync(file);
-                if (project == null)
-                {
-                    var doc = _workspace.GetDocument(file);
-                    //implies the document doesnot exist in the workspace currently
-                    var newProject = ProjectInfo.Create(
-                        filePath: file,
-                        id: ProjectId.CreateNewId(),
-                        version: VersionStamp.Create(),
-                        name: Path.GetFileName(file),
-                        metadataReferences: new MetadataReference[] { MetadataReference.CreateFromFile((typeof(object).Assembly).Location) },
-                        assemblyName: assemblyName,
-                        //TODO: Ask if there should be other languages as well 
-                        language: Language);
+                AddFileToWorkSpace(file);
+            }
+        }
 
-                    _workspace.AddProject(newProject);
-                    _workspace.AddDocument(newProject.Id, file);
-                }
+        private async void AddFileToWorkSpace(string filename)
+        {
+            string assemblyName = Guid.NewGuid().ToString("N");
+            await _projectSystem.GetProjectModelAsync(filename);
+            var document = _workspace.GetDocument(filename);
+            if (document == null)
+            {
+                var newProject = ProjectInfo.Create(
+                   filePath: filename,
+                   id: ProjectId.CreateNewId(),
+                   version: VersionStamp.Create(),
+                   name: Path.GetFileName(filename),
+                   metadataReferences: new MetadataReference[] { MetadataReference.CreateFromFile((typeof(object).Assembly).Location) },
+                   assemblyName: assemblyName,
+                   //TODO: Ask if there should be other languages as well 
+                   language: Language);
+
+                _workspace.AddProject(newProject);
+                _workspace.AddDocument(newProject.Id, filename);
+
+                _logger.LogInformation($"Successfully added file '{filename}' to workspace");
             }
         }
     }
