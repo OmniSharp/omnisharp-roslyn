c422035d9bbbbc1e637c3e5c53aa362ef9d7f36a
Unknown <akagarw@microsoft.com>
2018-07-24 16:25:28 -0700

    Add more failing tests

--- tests/OmniSharp.MiscellanousFiles.Tests/DiagnosticsFacts.cs
+++ tests/OmniSharp.MiscellanousFiles.Tests/DiagnosticsFacts.cs
@@ -1,9 +1,12 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using OmniSharp.Mef;
 using OmniSharp.Models.CodeCheck;
 using OmniSharp.Models.FilesChanged;
+using OmniSharp.Models.SignatureHelp;
 using OmniSharp.Roslyn.CSharp.Services.Diagnostics;
 using OmniSharp.Roslyn.CSharp.Services.Files;
+using OmniSharp.Roslyn.CSharp.Services.Signatures;
 using TestUtility;
 using Xunit;
 using Xunit.Abstractions;
@@ -18,15 +21,63 @@ namespace OmniSharp.MiscellanousFiles.Tests
         }
 
         [Fact]
-        public async Task Returns_only_semantic_diagnotics()
+        public async Task Returns_only_syntactic_diagnotics()
         {
             using (var testProject = await TestAssets.Instance.GetTestProjectAsync("ProjectWithMiscFile"))
-            using (var host = CreateOmniSharpHost(testProject.Directory))
             {
-                var filePath = testProject.AddDisposableFile("a.cs", "class C { b a = new b(); int n  }");
-                var fileChangedService = host.GetRequestHandler<OnFilesChangedService>(OmniSharpEndpoints.FilesChanged);
-                await fileChangedService.Handle(new[]
+                var testFile = new TestFile("a.cs", "class C { b a = new b(); int n  }");
+                using (var host = await AddMiscellanousFile(testProject, testFile))
                 {
+                    var codeCheckService = host.GetRequestHandler<CodeCheckService>(OmniSharpEndpoints.CodeCheck);
+                    var quickFixes = await codeCheckService.Handle(new CodeCheckRequest());
+                    Assert.Single(quickFixes.QuickFixes);
+                    Assert.Equal("; expected", quickFixes.QuickFixes.First().Text);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Returns_signature_help()
+        {
+            using (var testProject = await TestAssets.Instance.GetTestProjectAsync("ProjectWithMiscFile"))
+            {
+                const string source =
+@"class Program
+{
+    public static void Main(){
+        System.Guid.NewGuid($$);
+    }
+}";
+                var testFile = new TestFile("a.cs", source);
+                using (var host = await AddMiscellanousFile(testProject, testFile))
+                {
+                    var service = host.GetRequestHandler<SignatureHelpService>(OmniSharpEndpoints.SignatureHelp);
+                    var point = testFile.Content.GetPointFromPosition();
+                    var request = new SignatureHelpRequest()
+                    {
+                        FileName = testFile.FileName,
+                        Line = point.Line,
+                        Column = point.Offset,
+                        Buffer = testFile.Content.Code
+                    };
+
+                    var actual = await service.Handle(request);
+                    Assert.Single(actual.Signatures);
+                    Assert.Equal(0, actual.ActiveParameter);
+                    Assert.Equal(0, actual.ActiveSignature);
+                    Assert.Equal("NewGuid", actual.Signatures.ElementAt(0).Name);
+                    Assert.Empty(actual.Signatures.ElementAt(0).Parameters);
+                }
+            }
+        }
+
+        private async Task<OmniSharpTestHost> AddMiscellanousFile(ITestProject testProject, TestFile testfile)
+        {
+            var host = CreateOmniSharpHost(testProject.Directory);
+            var filePath = testProject.AddDisposableFile(testfile.FileName, testfile.Content.Text.ToString());
+            var fileChangedService = host.GetRequestHandler<OnFilesChangedService>(OmniSharpEndpoints.FilesChanged);
+            await fileChangedService.Handle(new[]
+            {
                     new FilesChangedRequest
                     {
                         FileName = filePath,
@@ -34,14 +85,8 @@ namespace OmniSharp.MiscellanousFiles.Tests
                     }
                 });
 
-                await Task.Delay(2000);
-
-                var codeCheckService = host.GetRequestHandler<CodeCheckService>(OmniSharpEndpoints.CodeCheck);
-                var quickFixes = await codeCheckService.Handle(new CodeCheckRequest());
-
-                Assert.Single(quickFixes.QuickFixes);
-                Assert.Equal("; expected", quickFixes.QuickFixes.First().Text);
-            }
+            await Task.Delay(2000);
+            return host;
         }
     }
 }
