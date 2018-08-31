679fdd3a2d17b5740a024632f2822ba8b7584d82
Ravi Chande <rchande@microsoft.com>
2018-08-31 09:36:27 -0700

    Don't duplicate find symbols results from C# files

--- src/OmniSharp.Cake/Services/RequestHandlers/Navigation/FindSymbolsHandler.cs
+++ src/OmniSharp.Cake/Services/RequestHandlers/Navigation/FindSymbolsHandler.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Composition;
 using System.Threading.Tasks;
 using OmniSharp.Cake.Extensions;
+using OmniSharp.Extensions;
 using OmniSharp.Mef;
 using OmniSharp.Models;
 using OmniSharp.Models.FindSymbols;
@@ -14,6 +16,17 @@ namespace OmniSharp.Cake.Services.RequestHandlers.Navigation
         public FindSymbolsHandler(OmniSharpWorkspace workspace)
             : base(workspace)
         {
+        }
+
+        public override Task<QuickFixResponse> Handle(FindSymbolsRequest request)
+        {
+            Func<string, bool> isMatch =
+                candidate => request != null
+                ? candidate.IsValidCompletionFor(request.Filter)
+                : true;
+
+            return  Workspace.CurrentSolution.FindSymbols(isMatch,
+                p => p.Name.EndsWith(".cake", StringComparison.OrdinalIgnoreCase));
         }
 
         protected override Task<QuickFixResponse> TranslateResponse(QuickFixResponse response, FindSymbolsRequest request)
--- src/OmniSharp.Roslyn.CSharp/Services/Navigation/FindSymbolsService.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/Navigation/FindSymbolsService.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Composition;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.FindSymbols;
 using OmniSharp.Extensions;
 using OmniSharp.Mef;
 using OmniSharp.Models;
@@ -30,57 +27,8 @@ namespace OmniSharp.Roslyn.CSharp.Services.Navigation
                 ? candidate.IsValidCompletionFor(request.Filter)
                 : true;
 
-            return await FindSymbols(isMatch);
+            return await _workspace.CurrentSolution.FindSymbols(isMatch,
+                p => !p.Name.EndsWith(".cake", StringComparison.OrdinalIgnoreCase));
         }
-
-        private async Task<QuickFixResponse> FindSymbols(Func<string, bool> predicate)
-        {
-            var symbols = await SymbolFinder.FindSourceDeclarationsAsync(_workspace.CurrentSolution, predicate, SymbolFilter.TypeAndMember);
-
-            var symbolLocations = new List<QuickFix>();
-            foreach(var symbol in symbols)
-            {
-                // for partial methods, pick the one with body
-                var s = symbol;
-                if (s is IMethodSymbol method)
-                {
-                    s = method.PartialImplementationPart ?? symbol;
-                }
-
-                foreach (var location in s.Locations)
-                {
-                    symbolLocations.Add(ConvertSymbol(symbol, location));
-                }
-            }
-
-            return new QuickFixResponse(symbolLocations.Distinct());
-        }
-
-        private QuickFix ConvertSymbol(ISymbol symbol, Location location)
-        {
-            var lineSpan = location.GetLineSpan();
-            var path = lineSpan.Path;
-            var documents = _workspace.GetDocuments(path);
-
-            var format = SymbolDisplayFormat.MinimallyQualifiedFormat;
-            format = format.WithMemberOptions(format.MemberOptions
-                                              ^ SymbolDisplayMemberOptions.IncludeContainingType
-                                              ^ SymbolDisplayMemberOptions.IncludeType);
-
-            format = format.WithKindOptions(SymbolDisplayKindOptions.None);
-
-            return new SymbolLocation
-            {
-                Text = symbol.ToDisplayString(format),
-                Kind = symbol.GetKind(),
-                FileName = path,
-                Line = lineSpan.StartLinePosition.Line,
-                Column = lineSpan.StartLinePosition.Character,
-                EndLine = lineSpan.EndLinePosition.Line,
-                EndColumn = lineSpan.EndLinePosition.Character,
-                Projects = documents.Select(document => document.Project.Name).ToArray()
-            };
-        }
-
     }
 }
