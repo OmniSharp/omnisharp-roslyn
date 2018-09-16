f0a1f40d92069782216f988105e503a3eb431ca8
Andre Cato <acato@vertafore.com>
2018-09-16 13:48:02 -0400

    Update GetTestCommandResponse and add configuration service reference

--- src/OmniSharp.Abstractions/Models/v1/TestCommand/GetTestCommandResponse.cs
+++ src/OmniSharp.Abstractions/Models/v1/TestCommand/GetTestCommandResponse.cs
@@ -1,8 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace OmniSharp.Models.TestCommand
 {
-    public class GetTestCommandResponse
+    public class GetTestCommandResponse : IAggregateResponse
     {
         public string Directory { get; set; }
         public string TestCommand { get; set; }
+
+        public IEnumerable<QuickFix> QuickFixes { get; set; }
+
+        public IAggregateResponse Merge(IAggregateResponse response)
+        {
+            var quickFixResponse = (QuickFixResponse)response;
+            return new QuickFixResponse(this.QuickFixes.Concat(quickFixResponse.QuickFixes));
+        }
     }
 }
