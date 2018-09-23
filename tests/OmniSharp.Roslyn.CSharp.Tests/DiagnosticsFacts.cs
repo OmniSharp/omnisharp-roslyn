862d8de36817ff3517536375957537f9490a9e33
Savpek <savolainen.pekka@gmail.com>
2018-09-23 18:14:05 +0300

    Testfixes and added test that IDE analyzers are enabled.

--- tests/OmniSharp.Roslyn.CSharp.Tests/DiagnosticsFacts.cs
+++ tests/OmniSharp.Roslyn.CSharp.Tests/DiagnosticsFacts.cs
@@ -25,7 +25,7 @@ namespace OmniSharp.Roslyn.CSharp.Tests
             var requestHandler = GetRequestHandler(SharedOmniSharpTestHost);
             var quickFixes = await requestHandler.Handle(new CodeCheckRequest() { FileName = "a.cs" });
 
-            Assert.Single(quickFixes.QuickFixes);
+            Assert.Contains(quickFixes.QuickFixes.Select(x => x.ToString()), x => x.Contains("CS0029"));
             Assert.Equal("a.cs", quickFixes.QuickFixes.First().FileName);
         }
 
@@ -39,7 +39,20 @@ namespace OmniSharp.Roslyn.CSharp.Tests
                 new TestFile("b.cs", "class C2 { int n = true; }"));
 
             var quickFixes = await handler.Handle(new CodeCheckRequest());
-            Assert.Equal(2, quickFixes.QuickFixes.Count());
+            Assert.Contains(quickFixes.QuickFixes, x => x.Text.Contains("CS0029") && x.FileName == "a.cs");
+            Assert.Contains(quickFixes.QuickFixes, x => x.Text.Contains("CS0029") && x.FileName == "b.cs");
+        }
+
+        [Fact]
+        public async Task AnalysisSupportBuiltInIDEAnalysers()
+        {
+            var handler = GetRequestHandler(SharedOmniSharpTestHost);
+
+            SharedOmniSharpTestHost.AddFilesToWorkspace(
+                new TestFile("a.cs", "class C1 { int n = true; }"));
+
+            var quickFixes = await handler.Handle(new CodeCheckRequest());
+            Assert.Contains(quickFixes.QuickFixes, x => x.Text.Contains("IDE0040"));
         }
     }
 }
