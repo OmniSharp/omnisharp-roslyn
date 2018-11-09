8c2c8b482d8d9930757d4a5e93cc4b085ca43f1e
Andre Cato <acato@vertafore.com>
2018-11-09 09:54:38 -0500

    Go back to nunit since it runs 64 bit

--- src/OmniSharp.Roslyn.CSharp/Services/TestCommands/TestCommandService.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/TestCommands/TestCommandService.cs
@@ -78,7 +78,7 @@ namespace OmniSharp.Roslyn.CSharp.Services.TestCommands
                         break;
                 }
 
-                testCommand = testCommand.Replace("{{ProjectPath}}", document.Project.FilePath)
+                testCommand = testCommand.Replace("{{AssemblyPath}}", document.Project.OutputFilePath)
                     .Replace("{{TypeName}}", response.TestCommand);
                     //.Replace("{{MethodName}}", context.Symbol.OriginalDefinition.Locations.ToString());
 
