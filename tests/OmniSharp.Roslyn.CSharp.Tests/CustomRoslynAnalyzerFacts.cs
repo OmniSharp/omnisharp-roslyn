65c13981093d51dd83c33b07561445a68a04f1f2
Savpek <savolainen.pekka@gmail.com>
2019-01-19 21:10:09 +0200

    Attempt to remove retry from asserts.

--- tests/OmniSharp.Roslyn.CSharp.Tests/CustomRoslynAnalyzerFacts.cs
+++ tests/OmniSharp.Roslyn.CSharp.Tests/CustomRoslynAnalyzerFacts.cs
@@ -16,10 +16,8 @@ using Xunit.Sdk;
 
 namespace OmniSharp.Roslyn.CSharp.Tests
 {
-    public class CustomRoslynAnalyzerFacts : AbstractSingleRequestHandlerTestFixture<CodeCheckService>
+    public class CustomRoslynAnalyzerFacts
     {
-        protected override string EndpointName => OmniSharpEndpoints.CodeCheck;
-
         public class TestAnalyzerReference : AnalyzerReference
         {
             private readonly string _id;
@@ -91,72 +89,85 @@ namespace OmniSharp.Roslyn.CSharp.Tests
             }
         }
 
-        public CustomRoslynAnalyzerFacts(ITestOutputHelper output, SharedOmniSharpHostFixture sharedOmniSharpHostFixture)
-            : base(output, sharedOmniSharpHostFixture)
+        private readonly ITestOutputHelper _testOutput;
+
+        public CustomRoslynAnalyzerFacts(ITestOutputHelper testOutput)
         {
+            _testOutput = testOutput;
         }
 
         [Fact]
         public async Task When_custom_analyzers_are_executed_then_return_results()
         {
-            var testFile = new TestFile("testFile.cs", "class _this_is_invalid_test_class_name { int n = true; }");
-            var codeCheckService = GetRequestHandler(SharedOmniSharpTestHost);
-
-            SharedOmniSharpTestHost.AddFilesToWorkspace(testFile);
-
-            var testAnalyzerRef = new TestAnalyzerReference("TS1234");
-
-            var projectIds = CreateProjectWitFile(testFile, testAnalyzerRef);
-
-            // This retry should be replaced with emitted events listener aproach after LSP is mainstream.
-            // Retry is required when build system greatly slows down some times, another issue is that
-            // it feels that some of update events from workspace get lost and it requires further investigation.
-            // If missing events are true then same issue will happen with LSP version too.
-            await RetryAssert.On<ContainsException>(async () =>
+            using (var host = GetHost())
             {
-                var result = await codeCheckService.Handle(new CodeCheckRequest());
+                var testFile = new TestFile("testFile.cs", "class _this_is_invalid_test_class_name { int n = true; }");
+
+                host.AddFilesToWorkspace(testFile);
+
+                var testAnalyzerRef = new TestAnalyzerReference("TS1234");
+
+                AddProjectWitFile(host, testFile, testAnalyzerRef);
+
+                // This retry should be replaced with emitted events listener aproach after LSP is mainstream.
+                // Retry is required when build system greatly slows down some times, another issue is that
+                // it feels that some of update events from workspace get lost and it requires further investigation.
+                // If missing events are true then same issue will happen with LSP version too.
+                // await RetryAssert.On<ContainsException>(async () =>
+                // {
+                var result = await host.RequestCodeCheckAsync("testFile.cs");
                 Assert.Contains(result.QuickFixes, f => f.Text.Contains(testAnalyzerRef.Id.ToString()));
-            });
+                // });
+            }
+        }
+
+        private OmniSharpTestHost GetHost()
+        {
+            return OmniSharpTestHost.Create(testOutput: _testOutput);
         }
 
         [Fact]
         public async Task Always_return_results_from_net_default_analyzers()
         {
-            var testFile = new TestFile("testFile_1.cs", "class SomeClass { int n = true; }");
-            var codeCheckService = GetRequestHandler(SharedOmniSharpTestHost);
-
-            CreateProjectWitFile(testFile);
-
-            await RetryAssert.On<ContainsException>(async () =>
+            using (var host = GetHost())
             {
-                var result = await codeCheckService.Handle(new CodeCheckRequest());
+                var testFile = new TestFile("testFile_1.cs", "class SomeClass { int n = true; }");
+
+                AddProjectWitFile(host, testFile);
+
+                // await RetryAssert.On<ContainsException>(async () =>
+                // {
+                var result = await host.RequestCodeCheckAsync();
                 Assert.Contains(result.QuickFixes.Where(x => x.FileName == testFile.FileName), f => f.Text.Contains("CS"));
-            });
+                // });
+            }
         }
 
         [Fact]
         public async Task When_rules_udpate_diagnostic_severity_then_show_them_with_new_severity()
         {
-            var testFile = new TestFile("testFile_2.cs", "class _this_is_invalid_test_class_name { int n = true; }");
-            var codeCheckService = GetRequestHandler(SharedOmniSharpTestHost);
-            var ruleService = SharedOmniSharpTestHost.GetExport<RulesetsForProjects>();
-
-            var testAnalyzerRef = new TestAnalyzerReference("TS1100");
-
-            var projectIds = CreateProjectWitFile(testFile, testAnalyzerRef);
-            var testRules = CreateRules(testAnalyzerRef, ReportDiagnostic.Hidden);
-
-            ruleService.AddOrUpdateRuleset(projectIds.Single(), new RuleSet(
-                "",
-                new ReportDiagnostic(),
-                testRules.ToImmutableDictionary(),
-                new ImmutableArray<RuleSetInclude>()));
-
-            await RetryAssert.On<ContainsException>(async () =>
+            using (var host = GetHost())
             {
-                var result = await codeCheckService.Handle(new CodeCheckRequest());
+                var testFile = new TestFile("testFile_2.cs", "class _this_is_invalid_test_class_name { int n = true; }");
+                var ruleService = host.GetExport<RulesetsForProjects>();
+
+                var testAnalyzerRef = new TestAnalyzerReference("TS1100");
+
+                var projectIds = AddProjectWitFile(host, testFile, testAnalyzerRef);
+                var testRules = CreateRules(testAnalyzerRef, ReportDiagnostic.Hidden);
+
+                ruleService.AddOrUpdateRuleset(projectIds.Single(), new RuleSet(
+                    "",
+                    new ReportDiagnostic(),
+                    testRules.ToImmutableDictionary(),
+                    new ImmutableArray<RuleSetInclude>()));
+
+                // await RetryAssert.On<ContainsException>(async () =>
+                // {
+                var result = await host.RequestCodeCheckAsync("testFile_2.cs");
                 Assert.Contains(result.QuickFixes.OfType<DiagnosticLocation>(), f => f.Text.Contains(testAnalyzerRef.Id.ToString()) && f.LogLevel == "Hidden");
-            });
+                // });
+            }
         }
 
         private static Dictionary<string, ReportDiagnostic> CreateRules(TestAnalyzerReference testAnalyzerRef, ReportDiagnostic diagnostic)
@@ -171,59 +182,64 @@ namespace OmniSharp.Roslyn.CSharp.Tests
         // This is important because hidden still allows code fixes to execute, not prevents it, for this reason suppressed analytics should not be returned at all.
         public async Task When_custom_rule_is_set_to_none_dont_return_results_at_all()
         {
-            var testFile = new TestFile("testFile_3.cs", "class _this_is_invalid_test_class_name { int n = true; }");
-            var codeCheckService = GetRequestHandler(SharedOmniSharpTestHost);
-            var ruleService = SharedOmniSharpTestHost.GetExport<RulesetsForProjects>();
+            using (var host = GetHost())
+            {
+                var testFile = new TestFile("testFile_3.cs", "class _this_is_invalid_test_class_name { int n = true; }");
 
-            var testAnalyzerRef = new TestAnalyzerReference("TS1101");
+                var ruleService = host.GetExport<RulesetsForProjects>();
 
-            var projectIds = CreateProjectWitFile(testFile, testAnalyzerRef);
+                var testAnalyzerRef = new TestAnalyzerReference("TS1101");
 
-            var testRules = CreateRules(testAnalyzerRef, ReportDiagnostic.Suppress);
+                var projectIds = AddProjectWitFile(host, testFile, testAnalyzerRef);
 
-            ruleService.AddOrUpdateRuleset(projectIds.Single(), new RuleSet(
-                "",
-                new ReportDiagnostic(),
-                testRules.ToImmutableDictionary(),
-                new ImmutableArray<RuleSetInclude>()));
+                var testRules = CreateRules(testAnalyzerRef, ReportDiagnostic.Suppress);
 
-            var result = await codeCheckService.Handle(new CodeCheckRequest());
-            Assert.DoesNotContain(result.QuickFixes, f => f.Text.Contains(testAnalyzerRef.Id.ToString()));
+                ruleService.AddOrUpdateRuleset(projectIds.Single(), new RuleSet(
+                    "",
+                    new ReportDiagnostic(),
+                    testRules.ToImmutableDictionary(),
+                    new ImmutableArray<RuleSetInclude>()));
+
+                var result = await host.RequestCodeCheckAsync("testFile_3.cs");
+                Assert.DoesNotContain(result.QuickFixes, f => f.Text.Contains(testAnalyzerRef.Id.ToString()));
+            }
         }
 
         [Fact]
         public async Task When_diagnostic_is_disabled_by_default_updating_rule_will_enable_it()
         {
-            var testFile = new TestFile("testFile_3.cs", "class _this_is_invalid_test_class_name { int n = true; }");
-            var codeCheckService = GetRequestHandler(SharedOmniSharpTestHost);
-            var ruleService = SharedOmniSharpTestHost.GetExport<RulesetsForProjects>();
-
-            var testAnalyzerRef = new TestAnalyzerReference("TS1101", isEnabledByDefault: false);
-
-            var projectIds = CreateProjectWitFile(testFile, testAnalyzerRef);
-
-            var testRules = CreateRules(testAnalyzerRef, ReportDiagnostic.Error);
-
-            ruleService.AddOrUpdateRuleset(projectIds.Single(), new RuleSet(
-                "",
-                new ReportDiagnostic(),
-                testRules.ToImmutableDictionary(),
-                new ImmutableArray<RuleSetInclude>()));
-
-            await RetryAssert.On<ContainsException>(async () =>
+            using (var host = GetHost())
             {
-                var result = await codeCheckService.Handle(new CodeCheckRequest());
+                var testFile = new TestFile("testFile_4.cs", "class _this_is_invalid_test_class_name { int n = true; }");
+                var ruleService = host.GetExport<RulesetsForProjects>();
+
+                var testAnalyzerRef = new TestAnalyzerReference("TS1101", isEnabledByDefault: false);
+
+                var projectIds = AddProjectWitFile(host, testFile, testAnalyzerRef);
+
+                var testRules = CreateRules(testAnalyzerRef, ReportDiagnostic.Error);
+
+                ruleService.AddOrUpdateRuleset(projectIds.Single(), new RuleSet(
+                    "",
+                    new ReportDiagnostic(),
+                    testRules.ToImmutableDictionary(),
+                    new ImmutableArray<RuleSetInclude>()));
+
+                // await RetryAssert.On<ContainsException>(async () =>
+                // {
+                var result = await host.RequestCodeCheckAsync("testFile_4.cs");
                 Assert.Contains(result.QuickFixes, f => f.Text.Contains(testAnalyzerRef.Id.ToString()));
-            });
+                // });
+            }
         }
 
-        private IEnumerable<ProjectId> CreateProjectWitFile(TestFile testFile, TestAnalyzerReference testAnalyzerRef = null)
+        private IEnumerable<ProjectId> AddProjectWitFile(OmniSharpTestHost host, TestFile testFile, TestAnalyzerReference testAnalyzerRef = null)
         {
             var analyzerReferences = testAnalyzerRef == null ? default :
                 new AnalyzerReference[] { testAnalyzerRef }.ToImmutableArray();
 
             return TestHelpers.AddProjectToWorkspace(
-                            SharedOmniSharpTestHost.Workspace,
+                            host.Workspace,
                             "project.csproj",
                             new[] { "netcoreapp2.1" },
                             new[] { testFile },
