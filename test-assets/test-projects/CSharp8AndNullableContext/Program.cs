b039a1695e5e418ace382cb4b8711c9c8e733df7
filipw <wojcieszyn@gmail.com>
2019-02-20 15:02:55 +0100

    added test for NullableContextOptions

---
+++ test-assets/test-projects/CSharp8AndNullableContext/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? foo = null;
+            string bar = null;
+            Console.WriteLine(foo + bar);
+        }
+    }
+}
