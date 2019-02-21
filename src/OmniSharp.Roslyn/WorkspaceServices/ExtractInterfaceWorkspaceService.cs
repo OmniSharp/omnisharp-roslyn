70d3dc98f64cc93b5e1c853905219c49bf7f5b62
Savpek <savolainen.pekka@gmail.com>
2019-02-21 19:31:00 +0200

    Fixes for extract interface.

--- src/OmniSharp.Roslyn/WorkspaceServices/ExtractInterfaceWorkspaceService.cs
+++ src/OmniSharp.Roslyn/WorkspaceServices/ExtractInterfaceWorkspaceService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Castle.DynamicProxy;
@@ -15,17 +16,33 @@ namespace OmniSharp
             // IPickMember and PickMemberResults are internal types -> workaround with proxy.
             // This service simply passes all members through as selected and doesn't try show UI.
             // When roslyn exposes this interface and members -> get rid of this workaround.
-            var resultTypeInternal = Assembly.Load("Microsoft.CodeAnalysis.ExtractInterface").GetType("Microsoft.CodeAnalysis.PickMembers.PickMembersResult");
+            var resultTypeInternal = Assembly.Load("Microsoft.CodeAnalysis.Features").GetType("Microsoft.CodeAnalysis.ExtractInterface.ExtractInterfaceOptionsResult");
+            var enumType = resultTypeInternal.GetNestedTypes().Single(x => x.Name == "ExtractLocation");
+
+            var toSameFileEnumValue = Enum.Parse(enumType, "SameFile");
+
+            var interfaceName = invocation.Arguments[3] ?? throw new InvalidOperationException($"{nameof(ExtractInterfaceWorkspaceService)} default interface name was null.");
 
             var resultObject = Activator.CreateInstance(resultTypeInternal, new object[] {
                     false, // isCancelled
-                    ((List<ISymbol>)invocation.Arguments[2]).ToImmutableArray(), // InterfaceMembers selected -> take all.
-                    invocation.Arguments[3], // Interface name, use default.
-                    $"{invocation.Arguments[3]}.cs", // FileName
-                    0 // to same file (enum)
+                    ((List<ISymbol>)invocation.Arguments[2]).ToImmutableArray(), // InterfaceMembers selected -> select all.
+                    interfaceName,
+                    $"{interfaceName}.cs",
+                    toSameFileEnumValue
                 });
 
-            invocation.ReturnValue = Task.Run(() => resultObject);
+            //Type taskGenericType = MakeGenerictTaskTypeFromResult(resultTypeInternal);
+            var fromResultMethod = typeof(Task).GetMethod("FromResult").MakeGenericMethod(resultTypeInternal).Invoke(null, new[] { resultObject });
+            invocation.ReturnValue = fromResultMethod;
+            //invocation.ReturnValue = taskGenericType.GetMethod("FromResult").Invoke(null, new[] { resultObject });
+        }
+
+        private static Type MakeGenerictTaskTypeFromResult(Type resultTypeInternal)
+        {
+            var taskType = typeof(Task<>);
+            Type[] typeArgs = { resultTypeInternal };
+            var taskGenericType = taskType.MakeGenericType(typeArgs);
+            return taskGenericType;
         }
     }
 }
