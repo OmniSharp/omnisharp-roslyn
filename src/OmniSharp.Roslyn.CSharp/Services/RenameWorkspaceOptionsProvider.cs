251f22073aa6fb7cecdbdf4bf36a31cb15a9d67e
filipw <wojcieszyn@gmail.com>
2019-05-14 11:12:02 +0200

    added RenameWorkspaceOptionsProvider

---
+++ src/OmniSharp.Roslyn.CSharp/Services/RenameWorkspaceOptionsProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Composition;
+using Microsoft.CodeAnalysis.Options;
+using OmniSharp.Options;
+using OmniSharp.Roslyn.Options;
+using RoslynRenameOptions = Microsoft.CodeAnalysis.Rename.RenameOptions;
+
+namespace OmniSharp.Roslyn.CSharp.Services
+{
+    [Export(typeof(IWorkspaceOptionsProvider)), Shared]
+    public class RenameWorkspaceOptionsProvider : IWorkspaceOptionsProvider
+    {
+        public OptionSet Process(OptionSet currentOptionSet, OmniSharpOptions omniSharpOptions) =>
+            currentOptionSet
+               .WithChangedOption(RoslynRenameOptions.RenameInComments, omniSharpOptions.RenameOptions.RenameInComments)
+               .WithChangedOption(RoslynRenameOptions.RenameInStrings, omniSharpOptions.RenameOptions.RenameInStrings)
+               .WithChangedOption(RoslynRenameOptions.RenameOverloads, omniSharpOptions.RenameOptions.RenameOverloads);
+    }
+}
