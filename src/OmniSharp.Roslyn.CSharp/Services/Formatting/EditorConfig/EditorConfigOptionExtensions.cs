381b5c046cd16a78a74f8a2b7de057b5a3b46c3c
filipw <wojcieszyn@gmail.com>
2019-06-13 14:09:22 +0200

    added ability to read and apply editorconfig options to the optionset

---
+++ src/OmniSharp.Roslyn.CSharp/Services/Formatting/EditorConfig/EditorConfigOptionExtensions.cs
@@ -0,0 +1,28 @@
+﻿// adapted from https://github.com/dotnet/format/blob/master/src/Utilities/EditorConfigOptionsApplier.cs
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.VisualStudio.CodingConventions;
+
+namespace OmniSharp.Roslyn.CSharp.Services.Formatting.EditorConfig
+{
+    internal static class EditorConfigOptionExtensions
+    {
+        public static async Task<OptionSet> WithEditorConfigOptions(this OptionSet optionSet, string path)
+        {
+            var codingConventionsManager = CodingConventionsManagerFactory.CreateCodingConventionsManager();
+            var optionsApplier = new EditorConfigOptionsApplier();
+            var context = await codingConventionsManager.GetConventionContextAsync(path, CancellationToken.None);
+
+            if (context != null && context.CurrentConventions != null)
+            {
+                return optionsApplier.ApplyConventions(optionSet, context.CurrentConventions, LanguageNames.CSharp);
+            }
+
+            return optionSet;
+        }
+    }
+}
---
+++ src/OmniSharp.Roslyn.CSharp/Services/Formatting/EditorConfig/EditorConfigOptionsApplier.cs
@@ -0,0 +1,85 @@
+// adapted from https://github.com/dotnet/format/blob/master/src/Utilities/EditorConfigOptionsApplier.cs
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.VisualStudio.CodingConventions;
+
+namespace OmniSharp.Roslyn.CSharp.Services.Formatting.EditorConfig
+{
+    internal class EditorConfigOptionsApplier
+    {
+        private readonly ImmutableArray<(IOption, OptionStorageLocation, MethodInfo)> _formattingOptionsWithStorage;
+
+        public EditorConfigOptionsApplier()
+        {
+            var commonOptionsType = typeof(FormattingOptions);
+            var csharpOptionsType = typeof(CSharpFormattingOptions);
+            _formattingOptionsWithStorage = GetOptionsWithStorageFromTypes(new[] { commonOptionsType, csharpOptionsType });
+        }
+
+        public OptionSet ApplyConventions(OptionSet optionSet, ICodingConventionsSnapshot codingConventions, string languageName)
+        {
+            foreach (var optionWithStorage in _formattingOptionsWithStorage)
+            {
+                if (TryGetConventionValue(optionWithStorage, codingConventions, out var value))
+                {
+                    var option = optionWithStorage.Item1;
+                    var optionKey = new OptionKey(option, option.IsPerLanguage ? languageName : null);
+                    optionSet = optionSet.WithChangedOption(optionKey, value);
+                }
+            }
+
+            return optionSet;
+        }
+
+        internal ImmutableArray<(IOption, OptionStorageLocation, MethodInfo)> GetOptionsWithStorageFromTypes(params Type[] formattingOptionTypes)
+        {
+            var optionType = typeof(IOption);
+            return formattingOptionTypes
+                .SelectMany(t => t.GetProperties(BindingFlags.Public | BindingFlags.Static | BindingFlags.GetProperty))
+                .Where(p => optionType.IsAssignableFrom(p.PropertyType))
+                .Select(p => (IOption)p.GetValue(null))
+                .Select(GetOptionWithStorage)
+                .Where(ows => ows.Item2 != null)
+                .ToImmutableArray();
+        }
+
+        internal (IOption, OptionStorageLocation, MethodInfo) GetOptionWithStorage(IOption option)
+        {
+            var editorConfigStorage = !option.StorageLocations.IsDefaultOrEmpty
+                ? option.StorageLocations.FirstOrDefault(IsEditorConfigStorage)
+                : null;
+            var tryGetOptionMethod = editorConfigStorage?.GetType().GetMethod("TryGetOption");
+            return (option, editorConfigStorage, tryGetOptionMethod);
+        }
+
+        internal static bool IsEditorConfigStorage(OptionStorageLocation storageLocation)
+        {
+            return storageLocation.GetType().FullName.StartsWith("Microsoft.CodeAnalysis.Options.EditorConfigStorageLocation");
+        }
+
+        internal static bool TryGetConventionValue((IOption, OptionStorageLocation, MethodInfo) optionWithStorage, ICodingConventionsSnapshot codingConventions, out object value)
+        {
+            var (option, editorConfigStorage, tryGetOptionMethod) = optionWithStorage;
+
+            value = null;
+
+            // EditorConfigStorageLocation no longer accepts a IReadOnlyDictionary<string, object>. All values should
+            // be string so we can convert it into a Dictionary<string, string>
+            var adjustedConventions = codingConventions.AllRawConventions.ToDictionary(kvp => kvp.Key, kvp => (string)kvp.Value);
+            var args = new object[] { option, adjustedConventions, option.Type, value };
+
+            var isOptionPresent = (bool)tryGetOptionMethod.Invoke(editorConfigStorage, args);
+            value = args[3];
+
+            return isOptionPresent;
+        }
+    }
+}
