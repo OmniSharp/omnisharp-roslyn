c21c3e93df94983023998dfb94f5a986f790e1c5
Savpek <savolainen.pekka@gmail.com>
2019-07-16 20:25:33 +0300

    Added missing attribute.

--- src/OmniSharp.Roslyn.CSharp/Services/Refactoring/GetFixAllCodeActionService.cs
+++ src/OmniSharp.Roslyn.CSharp/Services/Refactoring/GetFixAllCodeActionService.cs
@@ -1,23 +1,66 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using OmniSharp.Mef;
+using OmniSharp.Models;
+using OmniSharp.Roslyn.CSharp.Services.Refactoring.V2;
+using OmniSharp.Roslyn.CSharp.Workers.Diagnostics;
+using OmniSharp.Services;
 
-namespace OmniSharp.Roslyn.CSharp.Services
+namespace OmniSharp.Roslyn.CSharp.Services.Refactoring
 {
     [OmniSharpHandler(OmniSharpEndpoints.GetFixAll, LanguageNames.CSharp)]
-    public class GetFixAllCodeActionService : IRequestHandler<GetFixAllRequest, GetFixAllResponse>
+    public class GetFixAllCodeActionService : FixAllCodeActionBase, IRequestHandler<GetFixAllRequest, GetFixAllResponse>
     {
-        public Task<GetFixAllResponse> Handle(GetFixAllRequest request)
+        [ImportingConstructor]
+        public GetFixAllCodeActionService(ICsDiagnosticWorker diagnosticWorker, CachingCodeFixProviderForProjects codeFixProvider, OmniSharpWorkspace workspace, [ImportMany] IEnumerable<ICodeActionProvider> providers) : base(diagnosticWorker, codeFixProvider, workspace, providers)
         {
-            return Task.FromResult(new GetFixAllResponse());
+        }
+
+        public async Task<GetFixAllResponse> Handle(GetFixAllRequest request)
+        {
+            var allProjectIds = Workspace.CurrentSolution.Projects.Select(x => x.Id);
+
+            var availableFixes = await Task.WhenAll(allProjectIds.Select(id => GetAvailableCodeFixes(id)));
+
+            var distinctDiagnosticsThatCanBeFixed = availableFixes
+                .SelectMany(x => x)
+                .SelectMany(x => x.MatchingDiagnostics)
+                .GroupBy(x => x.Id)
+                .Select(x => x.First())
+                .Select(x => new FixAllItem(x.Id, x.GetMessage()))
+                .ToArray();
+
+            return new GetFixAllResponse(distinctDiagnosticsThatCanBeFixed);
         }
     }
 
     public class GetFixAllResponse
     {
+        public GetFixAllResponse(IEnumerable<FixAllItem> fixableItems)
+        {
+            Items = fixableItems;
+        }
+
+        public IEnumerable<FixAllItem> Items { get; set; }
     }
 
-    public class GetFixAllRequest
+    public class FixAllItem
+    {
+        public FixAllItem(string id, string message)
+        {
+            Id = id;
+            Message = message;
+        }
+
+        public string Id { get; set; }
+        public string Message { get; set; }
+    }
+
+    [OmniSharpEndpoint(OmniSharpEndpoints.GetFixAll, typeof(GetFixAllRequest), typeof(GetFixAllResponse))]
+    public class GetFixAllRequest: SimpleFileRequest
     {
     }
 }
