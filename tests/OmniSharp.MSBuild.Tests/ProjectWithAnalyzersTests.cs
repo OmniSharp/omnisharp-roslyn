ed02128d46ae1a626747dea06752aee156741585
filipw <wojcieszyn@gmail.com>
2019-08-12 20:33:59 +0200

    moved ProjectWithComplexAnalyzersTests to its own class

--- tests/OmniSharp.MSBuild.Tests/ProjectWithAnalyzersTests.cs
+++ tests/OmniSharp.MSBuild.Tests/ProjectWithAnalyzersTests.cs
@@ -40,23 +40,6 @@ namespace OmniSharp.MSBuild.Tests
         }
 
         [Fact]
-        public async Task CanLoadComplexAnalyzers()
-        {
-            using (var testProject = await TestAssets.Instance.GetTestProjectAsync("ProjectWithComplexAnalyzers"))
-            using (var host = CreateMSBuildTestHost(testProject.Directory, configurationData: TestHelpers.GetConfigurationDataWithAnalyzerConfig(roslynAnalyzersEnabled: true)))
-            {
-                await host.RestoreProject(testProject);
-
-                var diagnostics = await host.RequestCodeCheckAsync(Path.Combine(testProject.Directory, "Program.cs"));
-
-                Assert.NotEmpty(diagnostics.QuickFixes);
-                Assert.Contains(diagnostics.QuickFixes, x => x.ToString().Contains("CA1303"));
-                // warning CA1303: Method 'void Program.Main(string[] args)' passes a literal string as parameter 'value' of a call to 'void Console.WriteLine(string value)'. Retrieve the following string(s) from
-                // a resource table instead: "Hello World!"
-            }
-        }
-
-        [Fact]
         public async Task WhenProjectHasAnalyzersItDoesntLockAnalyzerDlls()
         {
             using (var testProject = await TestAssets.Instance.GetTestProjectAsync("ProjectWithAnalyzers"))
---
+++ tests/OmniSharp.MSBuild.Tests/ProjectWithComplexAnalyzersTests.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using TestUtility;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OmniSharp.MSBuild.Tests
+{
+    public class ProjectWithComplexAnalyzersTests : AbstractMSBuildTestFixture
+    {
+        public ProjectWithComplexAnalyzersTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task CanLoadComplexAnalyzers()
+        {
+            using (var testProject = await TestAssets.Instance.GetTestProjectAsync("ProjectWithComplexAnalyzers"))
+            using (var host = CreateMSBuildTestHost(testProject.Directory, configurationData: TestHelpers.GetConfigurationDataWithAnalyzerConfig(roslynAnalyzersEnabled: true)))
+            {
+                await host.RestoreProject(testProject);
+
+                var diagnostics = await host.RequestCodeCheckAsync(Path.Combine(testProject.Directory, "Program.cs"));
+
+                Assert.NotEmpty(diagnostics.QuickFixes);
+                Assert.Contains(diagnostics.QuickFixes, x => x.ToString().Contains("CA1303"));
+                // warning CA1303: Method 'void Program.Main(string[] args)' passes a literal string as parameter 'value' of a call to 'void Console.WriteLine(string value)'. Retrieve the following string(s) from
+                // a resource table instead: "Hello World!"
+            }
+        }
+    }
+}
