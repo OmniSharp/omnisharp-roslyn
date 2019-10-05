f0a501352c602b5e67b7b92975e95b852748371e
Bernhard Richter <bernhard.richter@gmail.com>
2019-10-05 12:21:16 +0200

    Use ConcurrentDictionary with original implementation

--- src/OmniSharp.Script/CachingScriptMetadataResolver.cs
+++ src/OmniSharp.Script/CachingScriptMetadataResolver.cs
@@ -29,13 +29,35 @@ namespace OmniSharp.Script
 
         public override PortableExecutableReference ResolveMissingAssembly(MetadataReference definition, AssemblyIdentity referenceIdentity)
         {
-            return MissingReferenceCache.GetOrAdd(referenceIdentity.Name, _ => _defaultReferenceResolver.ResolveMissingAssembly(definition, referenceIdentity));
+            if (MissingReferenceCache.ContainsKey(referenceIdentity.Name))
+            {
+                return MissingReferenceCache[referenceIdentity.Name];
+            }
+
+            var result = _defaultReferenceResolver.ResolveMissingAssembly(definition, referenceIdentity);
+            if (result != null)
+            {
+                MissingReferenceCache[referenceIdentity.Name] = result;
+            }
+
+            return result;
         }
 
         public override ImmutableArray<PortableExecutableReference> ResolveReference(string reference, string baseFilePath, MetadataReferenceProperties properties)
         {
             var key = $"{reference}-{baseFilePath}";
-            return DirectReferenceCache.GetOrAdd(key, _ => _defaultReferenceResolver.ResolveReference(reference, baseFilePath, properties));
+            if (DirectReferenceCache.ContainsKey(key))
+            {
+                return DirectReferenceCache[key];
+            }
+
+            var result = _defaultReferenceResolver.ResolveReference(reference, baseFilePath, properties);
+            if (result.Length > 0)
+            {
+                DirectReferenceCache[key] = result;
+            }
+
+            return result;
         }
     }
 }
