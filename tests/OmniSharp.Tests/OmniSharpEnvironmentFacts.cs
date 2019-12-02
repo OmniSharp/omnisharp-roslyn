3f4bcd40be8a4726178f0a3fa40cfa9f78364bb1
filipw <wojcieszyn@gmail.com>
2019-12-02 16:05:57 +0100

    added tests for CommandLineApplication

--- tests/OmniSharp.Tests/OmniSharpEnvironmentFacts.cs
+++ tests/OmniSharp.Tests/OmniSharpEnvironmentFacts.cs
@@ -1,10 +1,34 @@
 ﻿using Microsoft.Extensions.Logging;
 using OmniSharp.Services;
+using System.Linq;
 using TestUtility;
 using Xunit;
 
 namespace OmniSharp.Tests
 {
+    public class CommandLineApplicationFacts
+    {
+        [InlineData("-s", @"\path\to\my-solution\foo", "a=b")]
+        [InlineData("--source", @"\path\to\my-solution\foo", "a=b")]
+        [InlineData("-s", @"\path\to\my=solution\foo", "a=b")]
+        [InlineData("--source", @"\path\to\my=solution\foo", "a=b")]
+        [InlineData("a=b", "-s", @"\path\to\my-solution\foo")]
+        [InlineData("a=b", "--source", @"\path\to\my-solution\foo")]
+        [InlineData("a=b", "-s", @"\path\to\my=solution\foo")]
+        [InlineData("a=b", "--source", @"\path\to\my=solution\foo")]
+        [InlineData("a=b", null, null)]
+        [Theory]
+        public void AllowsEqualsSignInSolutionPath(string arg1, string arg2, string arg3)
+        {
+            var app = new CommandLineApplication();
+            app.OnExecute(() => 0);
+            app.Execute(new[] { arg1, arg2, arg3 });
+
+            Assert.Single(app.OtherArgs);
+            Assert.Equal("a=b", app.OtherArgs.First());
+        }
+    }
+
     public class OmniSharpEnvironmentFacts
     {
         [Fact]
