67f70804e60afbf9caa8c74e438f44a3d9279b8d
Joshua Brooks <samirat@github.com>
2020-02-11 12:23:05 -0500

    Add tests for runsettings

--- tests/OmniSharp.DotNetTest.Tests/RunTestFacts.cs
+++ tests/OmniSharp.DotNetTest.Tests/RunTestFacts.cs
@@ -223,5 +223,27 @@ namespace OmniSharp.DotNetTest.Tests
             Assert.Single(response.Results);
             Assert.NotEmpty(response.Results[0].StandardOutput);
         }
+
+        [Fact]
+        public async Task RunMSTestWithRunSettings()
+        {
+            await RunDotNetTestAsync(
+                MSTestProject,
+                methodName: "Main.Test.MainTest.CheckRunSettings",
+                testFramework: "mstest",
+                shouldPass: true,
+                useRunSettings: true);
+        }
+
+        [Fact]
+        public async Task RunMSTestWithoutRunSettings()
+        {
+            await RunDotNetTestAsync(
+                MSTestProject,
+                methodName: "Main.Test.MainTest.CheckRunSettings",
+                testFramework: "mstest",
+                shouldPass: false,
+                useRunSettings: false);
+        }
     }
 }
