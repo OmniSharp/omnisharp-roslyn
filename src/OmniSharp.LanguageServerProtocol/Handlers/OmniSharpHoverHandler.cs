6af37d5b614b2673675f885012b70be0d307db05
Fredric Silberberg <fred@silberberg.xyz>
2020-07-28 20:21:25 -0700

    Move the Hover LSP provider to use the new quickinfo service.

--- src/OmniSharp.LanguageServerProtocol/Handlers/OmniSharpHoverHandler.cs
+++ src/OmniSharp.LanguageServerProtocol/Handlers/OmniSharpHoverHandler.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using OmniSharp.Extensions.JsonRpc;
-using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
-using OmniSharp.Extensions.LanguageServer.Protocol.Server;
-using OmniSharp.Models.TypeLookup;
+using OmniSharp.Models;
 
 namespace OmniSharp.LanguageServerProtocol.Handlers
 {
@@ -16,14 +14,14 @@ namespace OmniSharp.LanguageServerProtocol.Handlers
         public static IEnumerable<IJsonRpcHandler> Enumerate(RequestHandlers handlers)
         {
             foreach (var (selector, handler) in handlers
-                .OfType<Mef.IRequestHandler<TypeLookupRequest, TypeLookupResponse>>())
+                .OfType<Mef.IRequestHandler<QuickInfoRequest, QuickInfoResponse>>())
                 if (handler != null)
                     yield return new OmniSharpHoverHandler(handler, selector);
         }
 
-        private readonly Mef.IRequestHandler<TypeLookupRequest, TypeLookupResponse> _definitionHandler;
+        private readonly Mef.IRequestHandler<QuickInfoRequest, QuickInfoResponse> _definitionHandler;
 
-        public OmniSharpHoverHandler(Mef.IRequestHandler<TypeLookupRequest, TypeLookupResponse> definitionHandler, DocumentSelector documentSelector)
+        public OmniSharpHoverHandler(Mef.IRequestHandler<QuickInfoRequest, QuickInfoResponse> definitionHandler, DocumentSelector documentSelector)
             : base(new HoverRegistrationOptions()
             {
                 DocumentSelector = documentSelector
@@ -34,12 +32,11 @@ namespace OmniSharp.LanguageServerProtocol.Handlers
 
         public async override Task<Hover> Handle(HoverParams request, CancellationToken token)
         {
-            var omnisharpRequest = new TypeLookupRequest()
+            var omnisharpRequest = new QuickInfoRequest()
             {
                 FileName = Helpers.FromUri(request.TextDocument.Uri),
                 Column = Convert.ToInt32(request.Position.Character),
                 Line = Convert.ToInt32(request.Position.Line),
-                IncludeDocumentation = true
             };
 
             var omnisharpResponse = await _definitionHandler.Handle(omnisharpRequest);
@@ -48,7 +45,7 @@ namespace OmniSharp.LanguageServerProtocol.Handlers
             {
                 // TODO: Range?  We don't currently have that!
                 // Range =
-                Contents = new MarkedStringsOrMarkupContent(new Container<MarkedString>(Helpers.EscapeMarkdown(omnisharpResponse.Type), Helpers.EscapeMarkdown(omnisharpResponse.Documentation)))
+                Contents = new MarkedStringsOrMarkupContent(new MarkupContent() { Value = omnisharpResponse.Markdown })
             };
         }
     }
