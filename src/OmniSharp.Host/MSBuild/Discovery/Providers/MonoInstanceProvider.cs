9b7330e522f097df79548dd47a3649eac7aed5c2
filipw <wojcieszyn@gmail.com>
2020-10-21 11:57:29 +0200

    fixed Mono MSBuild version reporting

--- src/OmniSharp.Host/MSBuild/Discovery/Providers/MonoInstanceProvider.cs
+++ src/OmniSharp.Host/MSBuild/Discovery/Providers/MonoInstanceProvider.cs
@@ -103,8 +103,6 @@ Try installing MSBuild into Mono (e.g. 'sudo apt-get install msbuild') to enable
             var localMSBuildPath = FindLocalMSBuildDirectory();
             if (localMSBuildPath != null)
             {
-                microsoftBuildPath = Path.Combine(localMSBuildPath, "Current", "Bin", "Microsoft.Build.dll");
-
                 var localRoslynPath = Path.Combine(localMSBuildPath, "Current", "Bin", "Roslyn");
                 propertyOverrides.Add("CscToolPath", localRoslynPath);
                 propertyOverrides.Add("CscToolExe", "csc.exe");
