a109f92991b955959e064c9c5526e2da7d4c816c
Fredric Silberberg <fred@silberberg.xyz>
2020-11-23 01:06:42 -0800

    Add documentation comment creation to the FormatAfterKeystrokeService
    IDocumentationCommentSnippetService was recently moved down to the Features layer, which means omnisharp can now take advantage of it and generate documentation comment snippets on typing. There are a couple of caveats:
    
    1. LSP's "/onTypingFormat" endpoint has no support for setting the cursor location after format, which means that the cursor ends up after the inserted block. This is fine for newlines inside doc comments, but awkward for the initial expansion. However, unless we want to add special support with a custom endpoint, we can't get around this (the custom endpoint is how Roslyn does it).
    2. The service itself is still internal, so I had to add a few new reflection points.
    
    Server-side of https://github.com/OmniSharp/omnisharp-vscode/issues/8. It also needs a vscode change, but it's a one-line change I'll submit later after we get this merged and I can write some integration tests.

--- tests/TestUtility/AssertUtils.cs
+++ tests/TestUtility/AssertUtils.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Text;
 using Xunit;
 
 namespace TestUtility
@@ -23,6 +25,25 @@ namespace TestUtility
         private static string TrimLines(string source)
         {
             return string.Join("\n", source.Split('\n').Select(s => s.Trim()));
+        }
+
+        // Taken from dotnet/roslyn, MIT License.
+        // https://github.com/dotnet/roslyn/blob/2834b74995bb66a7cb19cb09069c17812819afdc/src/Compilers/Test/Core/Assert/AssertEx.cs#L188-L203
+        public static void Equal(string expected, string actual)
+        {
+            if (string.Equals(expected, actual, StringComparison.Ordinal))
+            {
+                return;
+            }
+
+            var message = new StringBuilder();
+            message.AppendLine();
+            message.AppendLine("Expected:");
+            message.AppendLine(expected);
+            message.AppendLine("Actual:");
+            message.AppendLine(actual);
+
+            Assert.True(false, message.ToString());
         }
     }
 }
