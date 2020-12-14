0666d20245a79c03403308e46aeeef1ffdfdcd41
Joey Robichaud <jorobich@microsoft.com>
2020-12-14 14:26:00 -0800

    Fix SDK version test

--- tests/OmniSharp.MSBuild.Tests/ProjectLoadListenerTests.cs
+++ tests/OmniSharp.MSBuild.Tests/ProjectLoadListenerTests.cs
@@ -189,7 +189,7 @@ namespace OmniSharp.MSBuild.Tests
         }
 
         [Fact]
-        public async Task The_correct_sdk_version_is_emitted()
+        public async Task The_correct_sdk_version_is_emitted_NETCore2_1()
         {
             // Arrange
             var emitter = new ProjectLoadTestEventEmitter();
@@ -197,11 +197,23 @@ namespace OmniSharp.MSBuild.Tests
             using var testProject = await TestAssets.Instance.GetTestProjectAsync("NetCore21Project");
             using var host = CreateMSBuildTestHost(testProject.Directory, emitter.AsExportDescriptionProvider(LoggerFactory));
             Assert.Single(emitter.ReceivedMessages);
+            Assert.Equal(GetHashedFileExtension("2.1.811"), emitter.ReceivedMessages[0].SdkVersion);
+        }
+
+        [Fact]
+        public async Task The_correct_sdk_version_is_emitted_NETCore3_1()
+        {
+            // Arrange
+            var emitter = new ProjectLoadTestEventEmitter();
+
+            using var testProject = await TestAssets.Instance.GetTestProjectAsync("NetCore31Project");
+            using var host = CreateMSBuildTestHost(testProject.Directory, emitter.AsExportDescriptionProvider(LoggerFactory));
+            Assert.Single(emitter.ReceivedMessages);
             Assert.Equal(GetHashedFileExtension("3.1.403"), emitter.ReceivedMessages[0].SdkVersion);
         }
 
         [Fact]
-        public async Task The_correct_sdk_version_is_emitted_2()
+        public async Task The_correct_sdk_version_is_emitted_NET5()
         {
             // Arrange
             var emitter = new ProjectLoadTestEventEmitter();
