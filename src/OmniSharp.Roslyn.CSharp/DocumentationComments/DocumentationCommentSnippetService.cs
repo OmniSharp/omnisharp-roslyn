331add743401b51e29c2c3381879c9c0764e42b8
Filip W <wojcieszyn@gmail.com>
2020-12-26 05:51:50 +0100

    address nullability errors

--- src/OmniSharp.Roslyn.CSharp/DocumentationComments/DocumentationCommentSnippetService.cs
+++ src/OmniSharp.Roslyn.CSharp/DocumentationComments/DocumentationCommentSnippetService.cs
@@ -1,4 +1,4 @@
-﻿#nullable enable
+#nullable enable
 
 using System;
 using System.Reflection;
@@ -21,44 +21,47 @@ namespace OmniSharp.Roslyn.DocumentationComments
         /// <summary>
         /// IDocumentationCommentService HostLanguageServices.GetRequiredService<IDocumentationCommentService>()
         /// </summary>
-        private static MethodInfo s_getRequiredService;
+        private static readonly MethodInfo? s_getRequiredService;
         /// <summary>
         /// DocumentationCommentSnippet IDocumentationCommentService.GetDocumentationCommentSnippetOnCharacterTyped(SyntaxTree, SourceText, int, DocumentOptionSet, CancellationToken)
         /// </summary>
-        private static MethodInfo s_getDocumentationCommentSnippetOnCharacterTyped;
+        private static readonly MethodInfo? s_getDocumentationCommentSnippetOnCharacterTyped;
         /// <summary>
         /// DocumentationCommentSnippet IDocumentationCommentService.GetDocumentationCommentSnippetOnEnterTyped(SyntaxTree, SourceText, int, DocumentOptionSet, CancellationToken)
         /// </summary>
-        private static MethodInfo s_getDocumentationCommentSnippetOnEnterTyped;
+        private static readonly MethodInfo? s_getDocumentationCommentSnippetOnEnterTyped;
         /// <summary>
         /// TextSpan DocumentationCommentSnippet.SpanToReplace
         /// </summary>
-        private static PropertyInfo s_spanToReplace;
+        private static readonly PropertyInfo? s_spanToReplace;
         /// <summary>
         /// string DocumentationCommentSnippet.SnippetText
         /// </summary>
-        private static PropertyInfo s_snippetText;
+        private static readonly PropertyInfo? s_snippetText;
 
         static DocumentationCommentSnippetService()
         {
             var iDocumentationCommentSnippetServiceType = typeof(CompletionItem).Assembly.GetType("Microsoft.CodeAnalysis.DocumentationComments.IDocumentationCommentSnippetService");
-            s_getDocumentationCommentSnippetOnCharacterTyped = iDocumentationCommentSnippetServiceType.GetMethod(nameof(GetDocumentationCommentSnippetOnCharacterTyped));
-            s_getDocumentationCommentSnippetOnEnterTyped = iDocumentationCommentSnippetServiceType.GetMethod(nameof(GetDocumentationCommentSnippetOnEnterTyped));
+            s_getDocumentationCommentSnippetOnCharacterTyped = iDocumentationCommentSnippetServiceType?.GetMethod(nameof(GetDocumentationCommentSnippetOnCharacterTyped));
+            s_getDocumentationCommentSnippetOnEnterTyped = iDocumentationCommentSnippetServiceType?.GetMethod(nameof(GetDocumentationCommentSnippetOnEnterTyped));
 
             var documentationCommentSnippet = typeof(CompletionItem).Assembly.GetType("Microsoft.CodeAnalysis.DocumentationComments.DocumentationCommentSnippet");
-            s_spanToReplace = documentationCommentSnippet.GetProperty(nameof(DocumentationCommentSnippet.SpanToReplace));
-            s_snippetText = documentationCommentSnippet.GetProperty(nameof(DocumentationCommentSnippet.SnippetText));
+            s_spanToReplace = documentationCommentSnippet?.GetProperty(nameof(DocumentationCommentSnippet.SpanToReplace));
+            s_snippetText = documentationCommentSnippet?.GetProperty(nameof(DocumentationCommentSnippet.SnippetText));
 
-            s_getRequiredService = typeof(HostLanguageServices).GetMethod(nameof(HostLanguageServices.GetRequiredService)).MakeGenericMethod(iDocumentationCommentSnippetServiceType);
+            if (iDocumentationCommentSnippetServiceType is object)
+                s_getRequiredService = typeof(HostLanguageServices).GetMethod(nameof(HostLanguageServices.GetRequiredService))?.MakeGenericMethod(iDocumentationCommentSnippetServiceType);
         }
 
         public static DocumentationCommentSnippetService GetDocumentationCommentSnippetService(Document document)
         {
+            if (s_getRequiredService is null) throw new InvalidOperationException("Could not find GetRequiredService<IDocumentationCommentService> factory.");
             var service = s_getRequiredService.Invoke(document.Project.LanguageServices, Array.Empty<object>());
+            if (service is null) throw new InvalidOperationException("Could not retrieve an implementation of IDocumentationCommentService.");
             return new DocumentationCommentSnippetService(service);
         }
 
-        private object _underlying;
+        private readonly object _underlying;
 
         private DocumentationCommentSnippetService(object underlying)
         {
@@ -67,25 +70,28 @@ namespace OmniSharp.Roslyn.DocumentationComments
 
         public DocumentationCommentSnippet? GetDocumentationCommentSnippetOnCharacterTyped(SyntaxTree syntaxTree, SourceText text, int position, DocumentOptionSet options, CancellationToken cancellationToken)
         {
+            if (s_getDocumentationCommentSnippetOnCharacterTyped is null) throw new InvalidOperationException("Could not find IDocumentationCommentService.GetDocumentationCommentSnippetOnCharacterTyped(SyntaxTree, SourceText, int, DocumentOptionSet, CancellationToken).");
             var originalSnippet = s_getDocumentationCommentSnippetOnCharacterTyped.Invoke(_underlying, new object[] { syntaxTree, text, position, options, cancellationToken });
             return ConvertSnippet(originalSnippet);
         }
 
         public DocumentationCommentSnippet? GetDocumentationCommentSnippetOnEnterTyped(SyntaxTree syntaxTree, SourceText text, int position, DocumentOptionSet options, CancellationToken cancellationToken)
         {
+            if (s_getDocumentationCommentSnippetOnEnterTyped is null) throw new InvalidOperationException("Could not find IDocumentationCommentService.GetDocumentationCommentSnippetOnEnterTyped(SyntaxTree, SourceText, int, DocumentOptionSet, CancellationToken).");
             var originalSnippet = s_getDocumentationCommentSnippetOnEnterTyped.Invoke(_underlying, new object[] { syntaxTree, text, position, options, cancellationToken });
             return ConvertSnippet(originalSnippet);
         }
 
         private static DocumentationCommentSnippet? ConvertSnippet(object? originalSnippet)
         {
-            if (originalSnippet == null)
+            switch (originalSnippet)
             {
-                return null;
-            }
-            else
-            {
-                return new DocumentationCommentSnippet((TextSpan)s_spanToReplace.GetValue(originalSnippet), (string)s_snippetText.GetValue(originalSnippet));
+                case null:
+                    return null;
+                default:
+                    if (s_spanToReplace is null) throw new InvalidOperationException("Could not find DocumentationCommentSnippet.SpanToReplace.");
+                    if (s_snippetText is null) throw new InvalidOperationException("Could not find DocumentationCommentSnippet.SnippetText.");
+                    return new DocumentationCommentSnippet((TextSpan)s_spanToReplace.GetValue(originalSnippet)!, (string)s_snippetText.GetValue(originalSnippet)!);
             }
         }
     }
