29468d40892e0059d21b51de83bf8593c5e0278b
Joey Robichaud <jorobich@microsoft.com>
2021-06-17 21:15:50 -0700

    Update ICSharpCode.Decompiler to 7.1.0.6543

--- tests/OmniSharp.Roslyn.CSharp.Tests/AbstractGoToDefinitionFacts.cs
+++ tests/OmniSharp.Roslyn.CSharp.Tests/AbstractGoToDefinitionFacts.cs
@@ -160,13 +160,13 @@ class Test {
         public async Task ReturnsPartialMethodDefinitionWithBody(string filename)
         {
             var testFile = new TestFile(filename, @"
-    public partial class MyClass 
+    public partial class MyClass
     {
         public MyClass()
         {
             Met$$hod();
         }
-        
+
         partial void {|def:Method|}()
         {
             //do stuff
@@ -468,7 +468,7 @@ class Bar {
             // second comment should indicate we have decompiled
             var comments = compilationUnit.DescendantTrivia().Where(t => t.Kind() == SyntaxKind.SingleLineCommentTrivia).ToArray();
             Assert.NotNull(comments);
-            Assert.Equal("// Decompiled with ICSharpCode.Decompiler 7.0.0.6488", comments[1].ToString());
+            Assert.Equal("// Decompiled with ICSharpCode.Decompiler 7.1.0.6543", comments[1].ToString());
 
             // contrary to regular metadata, we should have methods with full bodies
             // this condition would fail if decompilation wouldn't work
