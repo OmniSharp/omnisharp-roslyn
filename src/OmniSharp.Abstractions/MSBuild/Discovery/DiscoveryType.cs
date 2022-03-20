985568744a23a298bbbf7394db997907e41afbeb
Joey Robichaud <jorobich@microsoft.com>
2022-03-20 15:25:11 -0700

    Remove unused code

--- src/OmniSharp.Abstractions/MSBuild/Discovery/DiscoveryType.cs
+++ src/OmniSharp.Abstractions/MSBuild/Discovery/DiscoveryType.cs
@@ -1,11 +1,7 @@
-﻿using System;
-
-namespace OmniSharp.MSBuild.Discovery
+﻿namespace OmniSharp.MSBuild.Discovery
 {
     public enum DiscoveryType
     {
-        [Obsolete("The StandAlone type is no longer supported.")]
-        StandAlone = 0,
         DeveloperConsole = 1,
         VisualStudioSetup = 2,
         Mono = 3,
