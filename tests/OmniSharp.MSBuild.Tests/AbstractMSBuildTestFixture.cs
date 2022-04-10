10947a752b68bf3e3b32e3e5ea978b8c4e2e1ef1
Joey Robichaud <jorobich@microsoft.com>
2022-04-10 14:05:43 -0700

    Build on .NET 7 SDK.

--- tests/OmniSharp.MSBuild.Tests/AbstractMSBuildTestFixture.cs
+++ tests/OmniSharp.MSBuild.Tests/AbstractMSBuildTestFixture.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Composition.Hosting.Core;
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OmniSharp.MSBuild.Discovery;
 using OmniSharp.Roslyn.Utilities;
@@ -13,10 +11,9 @@ using Xunit.Abstractions;
 
 namespace OmniSharp.MSBuild.Tests
 {
-    public abstract class AbstractMSBuildTestFixture : AbstractTestFixture, IDisposable
+    public abstract class AbstractMSBuildTestFixture : AbstractTestFixture
     {
         private readonly IAssemblyLoader _assemblyLoader;
-        private readonly IMSBuildLocator _msbuildLocator;
         private readonly IAnalyzerAssemblyLoader _analyzerAssemblyLoader;
 
         public AbstractMSBuildTestFixture(ITestOutputHelper output)
@@ -24,23 +21,16 @@ namespace OmniSharp.MSBuild.Tests
         {
             _assemblyLoader = new AssemblyLoader(this.LoggerFactory);
             _analyzerAssemblyLoader = ShadowCopyAnalyzerAssemblyLoader.Instance;
-            _msbuildLocator = MSBuildLocator.CreateDefault(this.LoggerFactory, _assemblyLoader, configuration: null);
-
-            // Some tests require MSBuild to be discovered early
-            // to ensure that the Microsoft.Build.* assemblies can be located
-            _msbuildLocator.RegisterDefaultInstance(this.LoggerFactory.CreateLogger("MSBuildTests"), dotNetInfo: null);
         }
 
-        public void Dispose()
-        {
-            (_msbuildLocator as IDisposable)?.Dispose();
-        }
-
-        protected OmniSharpTestHost CreateMSBuildTestHost(string path, IEnumerable<ExportDescriptorProvider> additionalExports = null,
+        protected OmniSharpTestHost CreateMSBuildTestHost(
+            string path,
+            IEnumerable<ExportDescriptorProvider> additionalExports = null,
             IConfiguration configurationData = null)
         {
             var environment = new OmniSharpEnvironment(path, logLevel: LogLevel.Trace);
-            var serviceProvider = TestServiceProvider.Create(this.TestOutput, environment, this.LoggerFactory, _assemblyLoader, _analyzerAssemblyLoader, _msbuildLocator,
+            using var msbuildLocator = MSBuildLocator.CreateDefault(this.LoggerFactory, _assemblyLoader, configurationData);
+            var serviceProvider = TestServiceProvider.Create(this.TestOutput, environment, this.LoggerFactory, _assemblyLoader, _analyzerAssemblyLoader, msbuildLocator,
                 configurationData);
 
             return OmniSharpTestHost.Create(serviceProvider, additionalExports);
