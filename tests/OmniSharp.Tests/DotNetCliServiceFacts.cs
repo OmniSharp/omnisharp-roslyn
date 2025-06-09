4d8b7f4aba3d1437a606056680c502fc303425ac
Joey Robichaud <jorobich@microsoft.com>
2025-06-09 10:50:46 -0700

    Update to latest SDKs

--- tests/OmniSharp.Tests/DotNetCliServiceFacts.cs
+++ tests/OmniSharp.Tests/DotNetCliServiceFacts.cs
@@ -7,7 +7,7 @@ namespace OmniSharp.Tests
 {
     public class DotNetCliServiceFacts : AbstractTestFixture
     {
-        private const string DotNetVersion = "10.0.100-preview.3.25201.16";
+        private const string DotNetVersion = "10.0.100-preview.4.25258.110";
         private int Major { get; }
         private int Minor { get; }
         private int Patch { get; }
